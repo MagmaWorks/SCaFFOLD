44eb383c0d87a4bf643645c20ace07ee48328a52
gcorre <corre.gregoire@gmail.com>
2019-10-23 12:31:43 +0100

    ColumnDesign Calc

---
+++ TestCalcs/Material.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCalcs
+{
+    public class Concrete
+    {
+        public string Name { get; set; }
+        public double Fc { get; set; }
+        public double E { get; set; }
+
+        public Concrete(string n, double f, double e)
+        {
+            Name = n;
+            Fc = f;
+            E = e;
+        }
+
+        public Concrete(string n)
+        {
+            Name = n;
+        }
+
+        public Concrete()
+        {
+
+        }
+    }
+
+    public class Steel
+    {
+        public string Name { get; set; }
+        public double Fy { get; set; }
+        public double E { get; set; }
+
+        public Steel(string n, double f, double e = 200)
+        {
+            Name = n;
+            Fy = f;
+            E = e;
+        }
+
+        public Steel(string n)
+        {
+            Name = n;
+        }
+
+        public Steel()
+        {
+
+        }
+    }
+}
