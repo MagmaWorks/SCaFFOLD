65de929e77c97ad5f456d1a3a112f387a338fa00
Alex Baalham <a.baalham@whitbywood.com>
2020-04-14 16:42:28 +0100

    Released as 0.4.5

--- Calcs/Properties/AssemblyInfo.cs
+++ Calcs/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.4.*")]
-[assembly: AssemblyFileVersion("0.4.4.0")]
+[assembly: AssemblyFileVersion("0.4.5.0")]
