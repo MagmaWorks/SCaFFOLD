96be2b8c66057857f511b35916fe69164041f32f
Dan Growns <d.growns@whitbywood.com>
2024-08-02 15:27:00 +0100

    Reference fix for AB

--- Scaffold/Scaffold.Calculations/PunchingShear.cs
+++ Scaffold/Scaffold.Calculations/PunchingShear.cs
@@ -1,34 +1,23 @@
 ﻿using Scaffold.Core.Enums;
 using Scaffold.Core.Interfaces;
 using Scaffold.Core.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Scaffold.Calculations
 {
     public class PunchingShear : ICalculation
     {
         public string Title { get => "test"; set => throw new NotImplementedException(); }
+        string ICalculation.Type { get; set; }
 
         public string Type => "punching";
 
         public CalcStatus Status => CalcStatus.None;
-
         public IEnumerable<Formula> GetFormulae()
         {
             throw new NotImplementedException();
         }
 
-        public IReadOnlyList<ICalcValue> GetInputs()
-        {
-            throw new NotImplementedException();
-        }
-
-        public IReadOnlyList<ICalcValue> GetOutputs()
+        public void Update()
         {
             throw new NotImplementedException();
         }
