c09b326505e42c35e3898a9171ac5fdad3b97356
kpne <kpn@kpne.dk>
2024-11-29 09:47:25 +0100

    Creep and shrinkage calc

---
+++ Scaffold.Calculations/ConcCreepAndShrinkage.cs
@@ -0,0 +1,35 @@
+﻿using Scaffold.Core.Attributes;
+using Scaffold.Core.Enums;
+using Scaffold.Core.Interfaces;
+using Scaffold.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scaffold.Calculations
+{
+    public class ConcCreepAndShrinkage : ICalculation
+    {
+        public string Title { get; set; } = "";
+        public string Type { get; set; } = "Concrete Creep and Shrinkage";
+
+        public CalcStatus Status { get; set; } = CalcStatus.None;
+
+        [InputCalcValue(@"\varphi(t,t_0)", "Notional Creep Coefficient")]
+        public double notionalCreepCoeff { get; } = 0;
+
+
+
+        public IEnumerable<Formula> GetFormulae()
+        {
+            return new List<Formula>();
+        }
+
+        public void Update()
+        {
+            
+        }
+    }
+}
