6bb36fcdcbf88d980fabbbee15c839fb5f36953b
kpne <kpn@kpne.dk>
2024-12-05 14:33:22 +0100

    Formula interface and cleanup

---
+++ Scaffold.Core/Interfaces/IFormula.cs
@@ -0,0 +1,14 @@
+﻿using Scaffold.Core.Images.Interfaces;
+using Scaffold.Core.Enums;
+
+namespace Scaffold.Core.Interfaces;
+
+public interface IFormula
+{
+    List<string> Expression { get; }
+    string Reference { get; }
+    string Narrative { get; }
+    string Conclusion { get; }
+    CalcStatus Status { get; } 
+    ICalcImage Image { get; }
+}
