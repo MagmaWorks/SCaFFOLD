7dad9f82341a24eebb2fc09b02c721d607ae8c94
kpne <kpn@kpne.dk>
2024-12-06 19:39:46 +0100

    pass on metadata when using operator

--- Scaffold.Core/CalcValues/CalcDouble.cs
+++ Scaffold.Core/CalcValues/CalcDouble.cs
@@ -12,8 +12,8 @@ public sealed class CalcDouble : CalcValue<double>
     public CalcDouble(string name, string symbol, double value, string unit = "")
         : this(name, value)
     {
-        Symbol = symbol;
-        Unit = unit;
+        Symbol = symbol?.Trim();
+        Unit = unit?.Trim();
     }
 
     public override bool TryParse(string strValue)
@@ -29,23 +29,41 @@ public sealed class CalcDouble : CalcValue<double>
 
     public static implicit operator double(CalcDouble value) => value.Value;
 
-    public static CalcDouble operator +(CalcDouble c1, CalcDouble c2)
+    public static CalcDouble operator +(CalcDouble x, CalcDouble y)
     {
-        return new CalcDouble(c1.Value + c2.Value);
+        (string name, string symbol, string unit) = OperatorMetadataHelper(x, y, '+');
+        return new CalcDouble(name, symbol, x.Value + y.Value, unit);
     }
 
-    public static CalcDouble operator -(CalcDouble c1, CalcDouble c2)
+    public static CalcDouble operator -(CalcDouble x, CalcDouble y)
     {
-        return new CalcDouble(c1.Value - c2.Value);
+        (string name, string symbol, string unit) = OperatorMetadataHelper(x, y, '-');
+        return new CalcDouble(name, symbol, x.Value - y.Value, unit);
     }
 
-    public static CalcDouble operator *(CalcDouble c1, CalcDouble c2)
+    public static CalcDouble operator *(CalcDouble x, CalcDouble y)
     {
-        return new CalcDouble(c1.Value * c2.Value);
+        (string name, string symbol, string unit) = OperatorMetadataHelper(x, y, '*');
+        if (!string.IsNullOrEmpty(unit))
+        {
+            unit = $"{unit}²";
+        }
+
+        return new CalcDouble(name, symbol, x.Value * y.Value, unit);
     }
 
-    public static CalcDouble operator /(CalcDouble c1, CalcDouble c2)
+    public static CalcDouble operator /(CalcDouble x, CalcDouble y)
     {
-        return new CalcDouble(c1.Value / c2.Value);
+        (string name, string symbol, string unit) = OperatorMetadataHelper(x, y, '/');
+        return new CalcDouble(name, symbol, x.Value / y.Value);
+    }
+
+    private static (string name, string symbol, string unit) OperatorMetadataHelper(CalcDouble x, CalcDouble y, char operation)
+    {
+        string name = string.IsNullOrEmpty(x.DisplayName) || string.IsNullOrEmpty(y.DisplayName)
+            ? string.Empty : $"{x.DisplayName} {operation} {y.DisplayName}";
+        string symbol = x.Symbol == y.Symbol ? x.Symbol : string.Empty;
+        string unit = x.Unit == y.Unit ? x.Unit : string.Empty;
+        return (name,  symbol, unit);
     }
 }
--- Scaffold.Tests/UnitTests/CalcDoubleTests.cs
+++ Scaffold.Tests/UnitTests/CalcDoubleTests.cs
@@ -33,6 +33,23 @@ namespace Scaffold.Tests.UnitTests
         }
 
         [Fact]
+        public void AdditionSameUnitOperatorTest()
+        {
+            // Assemble
+            var calcDouble1 = new CalcDouble("l1", "L", 4.5, "m");
+            var calcDouble2 = new CalcDouble("l2", "L", 5.5, "m");
+
+            // Act
+            CalcDouble result = calcDouble1 + calcDouble2;
+
+            // Assert
+            Assert.Equal(10.0, result.Value);
+            Assert.Equal("L", result.Symbol);
+            Assert.Equal("m", result.Unit);
+            Assert.Equal("l1 + l2", result.DisplayName);
+        }
+
+        [Fact]
         public void SubtractionOperatorTest()
         {
             // Assemble
@@ -44,6 +61,23 @@ namespace Scaffold.Tests.UnitTests
 
             // Assert
             Assert.Equal(-1.0, result.Value);
+        }
+
+        [Fact]
+        public void SubtractionSameUnitOperatorTest()
+        {
+            // Assemble
+            var calcDouble1 = new CalcDouble("l1", "L", 4.5, "m");
+            var calcDouble2 = new CalcDouble("l2", "L", 5.5, "m");
+
+            // Act
+            CalcDouble result = calcDouble1 - calcDouble2;
+
+            // Assert
+            Assert.Equal(-1, result.Value);
+            Assert.Equal("L", result.Symbol);
+            Assert.Equal("m", result.Unit);
+            Assert.Equal("l1 - l2", result.DisplayName);
         }
 
         [Fact]
@@ -61,6 +95,23 @@ namespace Scaffold.Tests.UnitTests
         }
 
         [Fact]
+        public void MultiplicationSameUnitOperatorTest()
+        {
+            // Assemble
+            var calcDouble1 = new CalcDouble("l1", "L", 4.5, "m");
+            var calcDouble2 = new CalcDouble("l2", "L", 5.5, "m");
+
+            // Act
+            CalcDouble result = calcDouble1 * calcDouble2;
+
+            // Assert
+            Assert.Equal(4.5 * 5.5, result.Value);
+            Assert.Equal("L", result.Symbol);
+            Assert.Equal("m²", result.Unit);
+            Assert.Equal("l1 * l2", result.DisplayName);
+        }
+
+        [Fact]
         public void DivisionOperatorTest()
         {
             // Assemble
@@ -72,6 +123,23 @@ namespace Scaffold.Tests.UnitTests
 
             // Assert
             Assert.Equal(4.5 / 5.5, result.Value);
+        }
+
+        [Fact]
+        public void DivisionSameUnitOperatorTest()
+        {
+            // Assemble
+            var calcDouble1 = new CalcDouble("l1", "L", 4.5, "m");
+            var calcDouble2 = new CalcDouble("l2", "L", 5.5, "m");
+
+            // Act
+            CalcDouble result = calcDouble1 / calcDouble2;
+
+            // Assert
+            Assert.Equal(4.5 / 5.5, result.Value);
+            Assert.Equal("L", result.Symbol);
+            Assert.True(string.IsNullOrEmpty(result.Unit));
+            Assert.Equal("l1 / l2", result.DisplayName);
         }
     }
 }
