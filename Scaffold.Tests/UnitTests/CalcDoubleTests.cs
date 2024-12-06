af0eb404668c75c4a2af9406a1d8d242c95083d7
kpne <kpn@kpne.dk>
2024-12-06 18:30:08 +0100

    implicit operator test

---
+++ Scaffold.Tests/UnitTests/CalcDoubleTests.cs
@@ -0,0 +1,21 @@
+﻿using Scaffold.Core.CalcValues;
+
+namespace Scaffold.Tests.UnitTests
+{
+    public class CalcDoubleTests
+    {
+        [Fact]
+        public void ImplicitOperatorTest()
+        {
+            // Assemble
+            var calcDouble1 = new CalcDouble(4.5);
+            var calcDouble2 = new CalcDouble(5.5);
+
+            // Act
+            double result = calcDouble1 + calcDouble2;
+
+            // Assert
+            Assert.Equal(10.0, result);
+        }
+    }
+}
