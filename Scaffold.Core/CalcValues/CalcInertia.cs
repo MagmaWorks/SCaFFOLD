a7fe7d673aa45b7c7d481bdb07c65defb5f08762
kpne <kpn@kpne.dk>
2024-12-09 17:19:09 +0100

    move helper to abstract class

--- Scaffold.Core/CalcValues/CalcInertia.cs
+++ Scaffold.Core/CalcValues/CalcInertia.cs
@@ -13,28 +13,26 @@ public sealed class CalcInertia : CalcQuantity<AreaMomentOfInertia>
     public CalcInertia(double value, AreaMomentOfInertiaUnit unit, string name, string symbol)
         : base(new AreaMomentOfInertia(value, unit), name, symbol) { }
 
-
-    public static implicit operator double(CalcInertia value) => value.Value;
-    public static implicit operator AreaMomentOfInertia(CalcInertia value) => value.Quantity;
-
     public static CalcInertia operator +(CalcInertia x, CalcInertia y)
     {
-        (string name, string symbol, AreaMomentOfInertiaUnit unit) = OperatorMetadataHelper(x, y, '+');
+        (string name, string symbol, AreaMomentOfInertiaUnit unit)
+            = OperatorMetadataHelper<AreaMomentOfInertiaUnit>(x, y, '+');
         return new CalcInertia(new AreaMomentOfInertia(x.Quantity.As(unit) + y.Quantity.As(unit),
             unit), name, symbol);
     }
 
     public static CalcInertia operator -(CalcInertia x, CalcInertia y)
     {
-        (string name, string symbol, AreaMomentOfInertiaUnit unit) = OperatorMetadataHelper(x, y, '-');
+        (string name, string symbol, AreaMomentOfInertiaUnit unit)
+            = OperatorMetadataHelper<AreaMomentOfInertiaUnit>(x, y, '-');
         return new CalcInertia(new AreaMomentOfInertia(x.Quantity.As(unit) - y.Quantity.As(unit),
             unit), name, symbol);
     }
 
     public static CalcDouble operator /(CalcInertia x, CalcInertia y)
     {
-        (string name, string _, AreaMomentOfInertiaUnit unit) = OperatorMetadataHelper(x, y, '/');
-        return new CalcDouble(name, string.Empty, x.Quantity / y.Quantity);
+        (string name, _, AreaMomentOfInertiaUnit _) = OperatorMetadataHelper<AreaMomentOfInertiaUnit>(x, y, '/');
+        return new CalcDouble(x.Quantity / y.Quantity, name, string.Empty);
     }
 
     public static CalcVolume operator /(CalcInertia x, CalcLength y)
@@ -62,14 +60,5 @@ public sealed class CalcInertia : CalcQuantity<AreaMomentOfInertia>
         AreaMomentOfInertiaUnit unit = x.Quantity.Unit;
         return new CalcLength(new Length(x.Quantity.As(unit) / y.Quantity.As(unit.GetEquivilantVolumeUnit()),
             unit.GetEquivilantLengthUnit()), name, "");
-    }
-
-    private static (string name, string symbol, AreaMomentOfInertiaUnit unit) OperatorMetadataHelper(CalcInertia x, CalcInertia y, char operation)
-    {
-        string name = string.IsNullOrEmpty(x.DisplayName) || string.IsNullOrEmpty(y.DisplayName)
-            ? string.Empty : $"{x.DisplayName}{operation}{y.DisplayName}";
-        string symbol = x.Symbol == y.Symbol ? x.Symbol : string.Empty;
-        AreaMomentOfInertiaUnit unit = x.Quantity.Unit;
-        return (name, symbol, unit);
     }
 }
