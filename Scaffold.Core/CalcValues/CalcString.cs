e155ad93e84b7a80af6f681831e8e4db48c024ca
kpne <kpn@kpne.dk>
2024-12-10 10:00:50 +0100

    CalcString

---
+++ Scaffold.Core/CalcValues/CalcString.cs
@@ -0,0 +1,21 @@
+﻿using Scaffold.Core.Abstract;
+
+namespace Scaffold.Core.CalcValues;
+
+public class CalcString : CalcValue<string>
+{
+    public CalcString(string value)
+        : base(value, null, string.Empty, string.Empty) { }
+
+    public CalcString(string value, string name)
+        : base(value, name, string.Empty, string.Empty) { }
+
+    public CalcString(string value, string name, string symbol, string unit = "")
+        : base(value, name, symbol, unit) { }
+
+    public static CalcString operator +(CalcString x, CalcString y)
+    {
+        (string name, string symbol, string unit) = OperatorMetadataHelper(x, y, '+');
+        return new CalcString(x.Value + y.Value, name, symbol, unit);
+    }
+}
