2a3ebb1dc99ed5ad4c163613c27326dc5fe59f11
Til Reinhardt <t.reinhardt@whitbywood.com>
2025-03-31 17:43:29 +0200

    Standard and material

--- Scaffold.Sections/CalcObjects/CalcStandard.cs
+++ Scaffold.Sections/CalcObjects/CalcStandard.cs
@@ -1,29 +1,28 @@
-﻿using MagmaWorks.Taxonomy.Materials;
-using MagmaWorks.Taxonomy.Materials.StandardMaterials;
-using MagmaWorks.Taxonomy.Serialization;
+﻿using MagmaWorks.Taxonomy.Serialization;
+using MagmaWorks.Taxonomy.Standards;
 using Scaffold.Core.Enums;
 using Scaffold.Core.Interfaces;
 
 namespace Scaffold.Core.CalcObjects
 {
-    public class CalcMaterial : ICalcValue, IStandardMaterial<CalcStandard>
+    public class CalcStandard : ICalcValue, IStandard
     {
         public string DisplayName { get; set; }
         public string Symbol { get; set; }
         public CalcStatus Status { get; set; }
-        public CalcStandard Standard { get; set; }
-        public MaterialType Type { get; set; }
+        public StandardBody Body { get; set; }
+        public string Title { get; set; }
 
         public bool TryParse(string strValue)
         {
             try
             {
-                CalcMaterial calcStandard = strValue.FromJson<CalcMaterial>();
+                CalcStandard calcStandard = strValue.FromJson<CalcStandard>();
                 DisplayName = calcStandard.DisplayName;
                 Symbol = calcStandard.Symbol;
                 Status = calcStandard.Status;
-                Standard = calcStandard.Standard;
-                Type = calcStandard.Type;
+                Body = calcStandard.Body;
+                Title = calcStandard.Title;
                 return true;
             }
             catch
---
+++ Scaffold.Tests/UnitTests/CalcObjects/CalcStandardTests.cs
@@ -0,0 +1,26 @@
+﻿using MagmaWorks.Taxonomy.Standards;
+using Scaffold.Core.CalcObjects;
+using Scaffold.Core.Enums;
+
+namespace Scaffold.Tests.UnitTests.CalcValues
+{
+    public class CalcStandardTests
+    {
+        [Fact]
+        public void ParseFromStringTest()
+        {
+            // Arrange
+            var standard = new CalcStandard();
+            string json = "{\r\n  \"$type\": \"Scaffold.Core.CalcObjects.CalcStandard, Scaffold.Sections\",\r\n  \"DisplayName\": \"A\",\r\n  \"Symbol\": \"A\",\r\n  \"Status\": \"None\",\r\n  \"Body\": \"EN\",\r\n  \"Title\": \"My favorite standard\"\r\n}";
+
+            // Act & Assert
+            Assert.False(standard.TryParse("invalid"));
+            Assert.True(standard.TryParse(json));
+            Assert.Equal("A", standard.DisplayName);
+            Assert.Equal("A", standard.Symbol);
+            Assert.Equal(CalcStatus.None, standard.Status);
+            Assert.Equal(StandardBody.EN, standard.Body);
+            Assert.Equal("My favorite standard", standard.Title);
+        }
+    }
+}
