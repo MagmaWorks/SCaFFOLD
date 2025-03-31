652807179d632283815250652eaee903a6ed599d
Til Reinhardt <t.reinhardt@whitbywood.com>
2025-03-31 16:55:42 +0200

    Initial commit

---
+++ Scaffold.Sections/CalcObjects/CalcMaterial.cs
@@ -0,0 +1,16 @@
+﻿using Scaffold.Core.Enums;
+using Scaffold.Core.Interfaces;
+
+namespace Scaffold.Core.CalcObjects
+{
+    internal class CalcMaterial : ICalcValue
+    {
+        public string DisplayName { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public string Symbol { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+        public CalcStatus Status => throw new System.NotImplementedException();
+
+        public bool TryParse(string strValue) => throw new System.NotImplementedException();
+        public string ValueAsString() => throw new System.NotImplementedException();
+    }
+}
