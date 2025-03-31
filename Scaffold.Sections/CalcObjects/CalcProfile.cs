3fe95d0fb430362e5cbc4239735567c19e9dcb08
kpne <kpn@kpne.dk>
2025-03-31 17:52:21 +0200

    CalcProfile

---
+++ Scaffold.Sections/CalcObjects/CalcProfile.cs
@@ -0,0 +1,40 @@
+﻿using MagmaWorks.Taxonomy.Profiles;
+using MagmaWorks.Taxonomy.Serialization;
+using Scaffold.Core.Enums;
+using Scaffold.Core.Interfaces;
+
+namespace Scaffold.Core.CalcObjects
+{
+    public class CalcProfile : ICalcValue
+    {
+        IProfile Profile { get; set; }
+        public virtual string DisplayName {
+            get
+            {
+                return ValueAsString();
+            }
+
+            set
+            {
+                TryParse(value);
+            }
+        }
+        public string Symbol { get; set; }
+        public CalcStatus Status { get; set; }
+
+        public bool TryParse(string strValue)
+        {
+            try
+            {
+                Profile = strValue.FromJson<IProfile>();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public string ValueAsString() => Profile.Description ?? "Invalid Profile";
+    }
+}
