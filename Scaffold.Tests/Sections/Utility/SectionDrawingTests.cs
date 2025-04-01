1c2cae91db6af9b3a8aa0c9477d8d62efe615961
kpne <kpn@kpne.dk>
2025-04-01 15:16:16 +0200

    test

---
+++ Scaffold.Tests/Sections/Utility/SectionDrawingTests.cs
@@ -0,0 +1,26 @@
+﻿using OasysUnits;
+using OasysUnits.Units;
+using MagmaWorks.Taxonomy.Profiles;
+using SkiaSharp;
+using Scaffold.Core.Utility;
+
+namespace Scaffold.Tests.UnitTests.CalcQuantities
+{
+    public class SectionDrawingTests
+    {
+        [Fact]
+        public void Draw500x800Rectangle()
+        {
+            // Arrange
+            var rectangle = new Rectangle(
+                new Length(500, LengthUnit.Millimeter),
+                new Length(800, LengthUnit.Millimeter));
+
+            // Act
+            string path = SectionDrawing.DrawProfileSvg(rectangle);
+
+            // Assert
+            Assert.NotNull(path);
+        }
+    }
+}
