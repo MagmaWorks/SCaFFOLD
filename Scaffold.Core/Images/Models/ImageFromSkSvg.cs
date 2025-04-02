ea7564dbd822016b750be0b78f2d79d54cfb8971
kpne <kpn@kpne.dk>
2025-04-02 17:11:53 +0200

    Concrete Section calculation with drawing

---
+++ Scaffold.Core/Images/Models/ImageFromSkSvg.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Text;
+using Scaffold.Core.Images.Interfaces;
+using SkiaSharp;
+using SKSvg = SkiaSharp.Extended.Svg.SKSvg;
+
+namespace Scaffold.Core.Images.Models;
+
+public class ImageFromSkSvg : ICalcImage
+{
+    public ImageFromSkSvg(SKSvg svg) => Svg = svg;
+    public ImageFromSkSvg(string svgText)
+    {
+        byte[] bytes = Encoding.UTF8.GetBytes(svgText);
+        MemoryStream stream = new MemoryStream(bytes);
+        Svg.Load(stream);
+    } 
+    public SKBitmap Bitmap => GetImage();
+    public SKSvg Svg { get; } = new SKSvg();
+    public SKBitmap GetImage() {
+        var image = SKImage.FromPicture(Svg.Picture,
+            new SKSizeI((int)Svg.CanvasSize.Width, (int)Svg.CanvasSize.Height));
+        return SKBitmap.FromImage(image);
+    }
+}
