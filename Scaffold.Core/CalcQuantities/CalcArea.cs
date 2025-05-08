f26d92743ddfbee171854c20487df4c99eaf1906
kpne <kpn@kpne.dk>
2025-05-08 17:47:34 +0200

    add comparison operators to concrete implementation

--- Scaffold.Core/CalcQuantities/CalcArea.cs
+++ Scaffold.Core/CalcQuantities/CalcArea.cs
@@ -21,6 +21,7 @@ public sealed class CalcArea : CalcQuantity<Area>
     , IDivisionOperators<CalcArea, CalcArea, CalcDouble>
     , IDivisionOperators<CalcArea, double, CalcArea>
     , IUnaryNegationOperators<CalcArea, CalcArea>
+    , IComparisonOperators<CalcArea, CalcArea, bool>
 #endif
 {
     public CalcArea(Area area, string name, string symbol = "")
@@ -109,6 +110,13 @@ public sealed class CalcArea : CalcQuantity<Area>
         return new CalcArea(x.Value / y, (AreaUnit)x.Quantity.Unit, x.DisplayName, x.Symbol);
     }
 
+    public static bool operator >(CalcArea left, CalcArea right) => GreaterThan(left, right);
+    public static bool operator >=(CalcArea left, CalcArea right) => GreaterOrEqualThan(left, right);
+    public static bool operator <(CalcArea left, CalcArea right) => LessThan(left, right);
+    public static bool operator <=(CalcArea left, CalcArea right) => LessOrEqualThan(left, right);
+    public static bool operator ==(CalcArea left, CalcArea right) => Equals(left, right);
+    public static bool operator !=(CalcArea left, CalcArea right) => !Equals(left, right);
+
     public static bool TryParse(string str, IFormatProvider provider, out CalcArea result)
     {
         if (Area.TryParse(str, provider, out Area quantity))
@@ -129,4 +137,7 @@ public sealed class CalcArea : CalcQuantity<Area>
     public static CalcArea Zero => new CalcArea(Area.Zero, string.Empty);
 
     public static CalcArea AdditiveIdentity => Zero;
+
+    public override bool Equals(object obj) => base.Equals(obj);
+    public override int GetHashCode() => base.GetHashCode();
 }
