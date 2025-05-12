70ad73ff7a6906826da1a75130ec551bfe12b521
kpne <kpn@kpne.dk>
2025-05-12 11:57:35 +0200

    update exception description

--- Scaffold.Core/Exceptions/ScaffoldException.cs
+++ Scaffold.Core/Exceptions/ScaffoldException.cs
@@ -1,9 +1,10 @@
 ﻿namespace Scaffold.Core.Exceptions;
 public class ScaffoldException : Exception
 {
-    /// <summary>
-    /// Inherit from this method to throw Scaffold Exceptions to be caught by the main programming.
-    /// </summary>
-    /// <param name="message"></param>
+    /// <exception cref="ScaffoldException">
+    ///     If anything else goes wrong, typically due to a bug or unhandled case.
+    ///     We wrap exceptions in <see cref="ScaffoldException" /> to allow you to distinguish
+    ///     Scaffold.Core exceptions from other exceptions.
+    /// </exception>
     public ScaffoldException(string message) : base(message) { }
 }
