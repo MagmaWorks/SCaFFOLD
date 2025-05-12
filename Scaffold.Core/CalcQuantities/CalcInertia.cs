2a6aa124eca997ff83a1cca68360c471a4545581
kpne <kpn@kpne.dk>
2025-05-12 14:57:13 +0200

    update CalcInertia with Generic Math with tests

--- Scaffold.Core/CalcQuantities/CalcInertia.cs
+++ Scaffold.Core/CalcQuantities/CalcInertia.cs
@@ -1,10 +1,27 @@
-﻿using Scaffold.Core.Abstract;
+﻿#if NET7_0_OR_GREATER
+using System.Numerics;
+#endif
+using Scaffold.Core.Abstract;
 using Scaffold.Core.CalcValues;
 using Scaffold.Core.Static;
 
 namespace Scaffold.Core.CalcQuantities;
 
 public sealed class CalcInertia : CalcQuantity<AreaMomentOfInertia>
+#if NET7_0_OR_GREATER
+    , IParsable<CalcInertia>
+    , IAdditionOperators<CalcInertia, CalcInertia, CalcInertia>
+    , IAdditionOperators<CalcInertia, double, CalcInertia>
+    , IAdditiveIdentity<CalcInertia, CalcInertia>
+    , ISubtractionOperators<CalcInertia, CalcInertia, CalcInertia>
+    , IMultiplyOperators<CalcInertia, double, CalcInertia>
+    , IDivisionOperators<CalcInertia, CalcLength, CalcVolume>
+    , IDivisionOperators<CalcInertia, CalcArea, CalcArea>
+    , IDivisionOperators<CalcInertia, CalcVolume, CalcLength>
+    , IDivisionOperators<CalcInertia, double, CalcInertia>
+    , IUnaryNegationOperators<CalcInertia, CalcInertia>
+    , IComparisonOperators<CalcInertia, CalcInertia, bool>
+#endif
 {
     public CalcInertia(AreaMomentOfInertia areaMomentOfInertia, string name, string symbol = "")
         : base(areaMomentOfInertia, name, symbol) { }
@@ -12,12 +29,27 @@ public sealed class CalcInertia : CalcQuantity<AreaMomentOfInertia>
     public CalcInertia(double value, AreaMomentOfInertiaUnit unit, string name, string symbol)
         : base(new AreaMomentOfInertia(value, unit), name, symbol) { }
 
+    #region AdditionOperators
     public static CalcInertia operator +(CalcInertia x, CalcInertia y)
     {
         (string name, string symbol, AreaMomentOfInertiaUnit unit)
             = OperatorMetadataHelper<AreaMomentOfInertiaUnit>(x, y, '+');
         return new CalcInertia(new AreaMomentOfInertia(x.Quantity.As(unit) + y.Quantity.As(unit),
             unit), name, symbol);
+    }
+
+    public static CalcInertia operator +(CalcInertia x, double y)
+    {
+        return new CalcInertia(x.Value + y, (AreaMomentOfInertiaUnit)x.Quantity.Unit, x.DisplayName, x.Symbol);
+    }
+
+    public static CalcInertia operator +(double x, CalcInertia y) => y + x;
+    #endregion
+
+    #region SubtractionOperators
+    public static CalcInertia operator -(CalcInertia x)
+    {
+        return new CalcInertia(-(AreaMomentOfInertia)x.Quantity, $"-{x.DisplayName}", x.Symbol);
     }
 
     public static CalcInertia operator -(CalcInertia x, CalcInertia y)
@@ -27,7 +59,22 @@ public sealed class CalcInertia : CalcQuantity<AreaMomentOfInertia>
         return new CalcInertia(new AreaMomentOfInertia(x.Quantity.As(unit) - y.Quantity.As(unit),
             unit), name, symbol);
     }
+    public static CalcInertia operator -(CalcInertia x, double y)
+    {
+        return new CalcInertia(x.Value - y, (AreaMomentOfInertiaUnit)x.Quantity.Unit, x.DisplayName, x.Symbol);
+    }
+    #endregion
 
+    #region MultiplicationOperators
+    public static CalcInertia operator *(CalcInertia x, double y)
+    {
+        return new CalcInertia(x.Value * y, (AreaMomentOfInertiaUnit)x.Quantity.Unit, x.DisplayName, x.Symbol);
+    }
+
+    public static CalcInertia operator *(double x, CalcInertia y) => y * x;
+    #endregion
+
+    #region DivisionOperators
     public static CalcDouble operator /(CalcInertia x, CalcInertia y)
     {
         (string name, _, AreaMomentOfInertiaUnit _) = OperatorMetadataHelper<AreaMomentOfInertiaUnit>(x, y, '/');
@@ -61,33 +108,58 @@ public sealed class CalcInertia : CalcQuantity<AreaMomentOfInertia>
             unit.GetEquivilantLengthUnit()), name, "");
     }
 
-    public static CalcInertia operator +(CalcInertia x, double y)
-    {
-        return new CalcInertia(x.Value + y, (AreaMomentOfInertiaUnit)x.Quantity.Unit, x.DisplayName, x.Symbol);
-    }
-
-    public static CalcInertia operator +(double x, CalcInertia y)
-    {
-        return y + x;
-    }
-
-    public static CalcInertia operator -(CalcInertia x, double y)
-    {
-        return new CalcInertia(x.Value - y, (AreaMomentOfInertiaUnit)x.Quantity.Unit, x.DisplayName, x.Symbol);
-    }
-
-    public static CalcInertia operator *(CalcInertia x, double y)
-    {
-        return new CalcInertia(x.Value * y, (AreaMomentOfInertiaUnit)x.Quantity.Unit, x.DisplayName, x.Symbol);
-    }
-
-    public static CalcInertia operator *(double x, CalcInertia y)
-    {
-        return y * x;
-    }
-
     public static CalcInertia operator /(CalcInertia x, double y)
     {
         return new CalcInertia(x.Value / y, (AreaMomentOfInertiaUnit)x.Quantity.Unit, x.DisplayName, x.Symbol);
+    }
+    #endregion
+
+    #region PowerOperators
+    public static ICalcQuantity operator ^(CalcInertia x, double y)
+    {
+        if (y == 0.5)
+        {
+            return x.Sqrt();
+        }
+
+        throw new MathException("CalcInertia can only be raised by the power of 0.5");
+    }
+    #endregion
+
+    #region ComparisonOperators
+    public static bool operator >(CalcInertia left, CalcInertia right) => GreaterThan(left, right);
+    public static bool operator >=(CalcInertia left, CalcInertia right) => GreaterOrEqualThan(left, right);
+    public static bool operator <(CalcInertia left, CalcInertia right) => LessThan(left, right);
+    public static bool operator <=(CalcInertia left, CalcInertia right) => LessOrEqualThan(left, right);
+    public static bool operator ==(CalcInertia left, CalcInertia right) => left.Equals(right);
+    public static bool operator !=(CalcInertia left, CalcInertia right) => !left.Equals(right);
+    #endregion
+
+    public static bool TryParse(string str, IFormatProvider provider, out CalcInertia result)
+    {
+        if (AreaMomentOfInertia.TryParse(str, provider, out AreaMomentOfInertia quantity))
+        {
+            result = new CalcInertia(quantity, string.Empty);
+            return true;
+        }
+
+        result = null;
+        return false;
+    }
+
+    public static CalcInertia Parse(string str, IFormatProvider provider)
+    {
+        return new CalcInertia(AreaMomentOfInertia.Parse(str, provider), string.Empty);
+    }
+
+    public static CalcInertia Zero => new CalcInertia(AreaMomentOfInertia.Zero, string.Empty);
+    public static CalcInertia AdditiveIdentity => Zero;
+    public override bool Equals(object obj) => base.Equals(obj);
+    public override int GetHashCode() => base.GetHashCode();
+    public CalcArea Sqrt()
+    {
+        AreaMomentOfInertiaUnit unit = (AreaMomentOfInertiaUnit)Quantity.Unit;
+        string name = string.IsNullOrEmpty(DisplayName) ? string.Empty : $"√{DisplayName}";
+        return new CalcArea(Math.Sqrt(Value), unit.GetEquivilantAreaUnit(), name, "");
     }
 }
--- Scaffold.Tests/UnitTests/CalcQuantities/CalcInertiaTests.cs
+++ Scaffold.Tests/UnitTests/CalcQuantities/CalcInertiaTests.cs
@@ -8,7 +8,7 @@ namespace Scaffold.Tests.UnitTests.CalcQuantities
     public class CalcInertiaTests
     {
         [Fact]
-        public void ParseFromStringTest()
+        public void TryParseFromStringTest()
         {
             // Arrange
             var calcInertia = new CalcInertia(4.5, AreaMomentOfInertiaUnit.FootToTheFourth, "inertia", "I");
@@ -18,6 +18,30 @@ namespace Scaffold.Tests.UnitTests.CalcQuantities
             Assert.True(calcInertia.TryParse("5.5 cm⁴"));
             Assert.Equal(5.5, calcInertia.Value);
             Assert.Equal("cm⁴", calcInertia.Unit);
+        }
+
+        [Fact]
+        public void ParseFromStringTest()
+        {
+            // Arrange
+            // Act
+            var calcInertia = CalcInertia.Parse("5.5 cm⁴", null);
+
+            // Assert
+            Assert.Equal(5.5, calcInertia.Value);
+            Assert.Equal("cm⁴", calcInertia.Unit);
+        }
+
+        [Fact]
+        public void TryParseFailureTest()
+        {
+            // Arrange
+            var calcQuantity = new CalcInertia(4.5, AreaMomentOfInertiaUnit.FootToTheFourth, "inertia", "I");
+
+            // Act
+            // Assert
+            Assert.False(CalcInertia.TryParse("two hundred horses", null, out calcQuantity));
+            Assert.Null(calcQuantity);
         }
 
         [Fact]
@@ -171,7 +195,7 @@ namespace Scaffold.Tests.UnitTests.CalcQuantities
         public void SubtractionDoubleOperatorTest()
         {
             // Arrange
-            var calcInertia = new CalcInertia(4.5, AreaMomentOfInertiaUnit.CentimeterToTheFourth, "a1", "I");
+            var calcInertia = new CalcInertia(4.5, AreaMomentOfInertiaUnit.CentimeterToTheFourth, "i1", "I");
 
             // Act
             CalcInertia result = calcInertia - 2;
@@ -180,8 +204,24 @@ namespace Scaffold.Tests.UnitTests.CalcQuantities
             Assert.Equal(4.5 - 2, result.Value);
             Assert.Equal("I", result.Symbol);
             Assert.Equal("cm⁴", result.Unit);
-            Assert.Equal("a1", result.DisplayName);
+            Assert.Equal("i1", result.DisplayName);
             Assert.Equal(4.5, calcInertia.Value);
+        }
+
+        [Fact]
+        public void UnaryNegationOperatorTest()
+        {
+            // Arrange
+            var calcInertia = new CalcInertia(4.5, AreaMomentOfInertiaUnit.CentimeterToTheFourth, "i1", "I");
+
+            // Act
+            CalcInertia result = -calcInertia;
+
+            // Assert
+            Assert.Equal(-4.5, result.Value);
+            Assert.Equal("I", result.Symbol);
+            Assert.Equal("cm⁴", result.Unit);
+            Assert.Equal("-i1", result.DisplayName);
         }
 
         [Fact]
@@ -216,6 +256,282 @@ namespace Scaffold.Tests.UnitTests.CalcQuantities
             Assert.Equal("cm⁴", result.Unit);
             Assert.Equal("a1", result.DisplayName);
             Assert.Equal(4.5, calcInertia.Value);
+        }
+
+        [Fact]
+        public void PowerOperatorTest()
+        {
+            // Arrange
+            var calcInertia = new CalcInertia(4.5, AreaMomentOfInertiaUnit.CentimeterToTheFourth, "i1", "I");
+
+            // Act
+            CalcArea result = (CalcArea)(calcInertia ^ 0.5);
+
+            // Assert
+            Assert.Equal(Math.Pow(4.5, 0.5), result.Value);
+            Assert.Equal("cm²", result.Unit);
+            Assert.Equal("√i1", result.DisplayName);
+            Assert.True(string.IsNullOrEmpty(result.Symbol));
+            Assert.Equal(4.5, calcInertia.Value);
+        }
+
+        [Fact]
+        public void RootTest()
+        {
+            // Arrange
+            var calcInertia = new CalcInertia(4.5, AreaMomentOfInertiaUnit.CentimeterToTheFourth, "i1", "I");
+
+            // Act
+            CalcArea result = calcInertia.Sqrt();
+
+            // Assert
+            Assert.Equal(Math.Sqrt(4.5), result.Value);
+            Assert.Equal("cm²", result.Unit);
+            Assert.Equal("√i1", result.DisplayName);
+            Assert.True(string.IsNullOrEmpty(result.Symbol));
+            Assert.Equal(4.5, calcInertia.Value);
+        }
+
+        [Fact]
+        public void SumTest()
+        {
+            // Arrange
+            var calcArea1 = new CalcInertia(1, AreaMomentOfInertiaUnit.CentimeterToTheFourth, "a", "A");
+            var calcArea2 = new CalcInertia(2, AreaMomentOfInertiaUnit.CentimeterToTheFourth, "a", "A");
+            var calcArea3 = new CalcInertia(3, AreaMomentOfInertiaUnit.CentimeterToTheFourth, "a", "A");
+            var areas = new List<CalcInertia>() { calcArea1, calcArea2, calcArea3 };
+
+            // Act
+            CalcInertia sum = areas.Sum();
+
+            // Assert
+            Assert.Equal(6, sum.Value);
+            Assert.Equal("cm⁴", sum.Unit);
+        }
+
+        [Fact]
+        public void AverageTest()
+        {
+            // Arrange
+            var calcArea1 = new CalcInertia(1, AreaMomentOfInertiaUnit.CentimeterToTheFourth, "a", "A");
+            var calcArea2 = new CalcInertia(2, AreaMomentOfInertiaUnit.CentimeterToTheFourth, "a", "A");
+            var calcArea3 = new CalcInertia(3, AreaMomentOfInertiaUnit.CentimeterToTheFourth, "a", "A");
+            var areas = new List<CalcInertia>() { calcArea1, calcArea2, calcArea3 };
+
+            // Act
+            CalcInertia sum = areas.Average();
+
+            // Assert
+            Assert.Equal(2, sum.Value);
+            Assert.Equal("cm⁴", sum.Unit);
+        }
+
+        [Theory]
+        [InlineData(4.3, 4.3, true)]
+        [InlineData(4.31, 4.3, false)]
+        public void EqualOperatorTest(double val1, double val2, bool expected)
+        {
+            // Arrange
+            var calcArea1 = new CalcInertia(val1, AreaMomentOfInertiaUnit.MeterToTheFourth, "q2", "Q");
+            var calcArea2 = new CalcInertia(val2 * 100000000, AreaMomentOfInertiaUnit.CentimeterToTheFourth, "q2", "Q");
+
+            // Act
+            bool result = calcArea1 == calcArea2;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(4.3, 4.3, false)]
+        [InlineData(4.31, 4.3, true)]
+        public void NotEqualOperatorTest(double val1, double val2, bool expected)
+        {
+            // Arrange
+            var calcArea1 = new CalcInertia(val1, AreaMomentOfInertiaUnit.MeterToTheFourth, "q2", "Q");
+            var calcArea2 = new CalcInertia(val2 * 100000000, AreaMomentOfInertiaUnit.CentimeterToTheFourth, "q2", "Q");
+
+            // Act
+            bool result = calcArea1 != calcArea2;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(4.3, 4.3, false)]
+        [InlineData(4.31, 4.3, true)]
+        public void GreaterThanOperatorTest(double val1, double val2, bool expected)
+        {
+            // Arrange
+            var calcArea1 = new CalcInertia(val1, AreaMomentOfInertiaUnit.MeterToTheFourth, "q2", "Q");
+            var calcArea2 = new CalcInertia(val2 * 100000000, AreaMomentOfInertiaUnit.CentimeterToTheFourth, "q2", "Q");
+
+            // Act
+            bool result = calcArea1 > calcArea2;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(4.3, 4.3, false)]
+        [InlineData(4.3, 4.31, true)]
+        public void LessThanOperatorTest(double val1, double val2, bool expected)
+        {
+            // Arrange
+            var calcArea1 = new CalcInertia(val1, AreaMomentOfInertiaUnit.MeterToTheFourth, "q2", "Q");
+            var calcArea2 = new CalcInertia(val2 * 100000000, AreaMomentOfInertiaUnit.CentimeterToTheFourth, "q2", "Q");
+
+            // Act
+            bool result = calcArea1 < calcArea2;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(4.3, 4.3, true)]
+        [InlineData(4.31, 4.3, true)]
+        [InlineData(4.3, 4.31, false)]
+        public void GreaterOrEqualOperatorTest(double val1, double val2, bool expected)
+        {
+            // Arrange
+            var calcArea1 = new CalcInertia(val1, AreaMomentOfInertiaUnit.MeterToTheFourth, "q2", "Q");
+            var calcArea2 = new CalcInertia(val2 * 100000000, AreaMomentOfInertiaUnit.CentimeterToTheFourth, "q2", "Q");
+
+            // Act
+            bool result = calcArea1 >= calcArea2;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(4.3, 4.3, true)]
+        [InlineData(4.3, 4.31, true)]
+        [InlineData(4.31, 4.30, false)]
+        public void LessOrEqualOperatorTest(double val1, double val2, bool expected)
+        {
+            // Arrange
+            var calcArea1 = new CalcInertia(val1, AreaMomentOfInertiaUnit.MeterToTheFourth, "q2", "Q");
+            var calcArea2 = new CalcInertia(val2 * 100000000, AreaMomentOfInertiaUnit.CentimeterToTheFourth, "q2", "Q");
+
+            // Act
+            bool result = calcArea1 <= calcArea2;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void EqualsReferenceEqualsObjectTest()
+        {
+            // Arrange
+            var calcArea = new CalcInertia(4.5, AreaMomentOfInertiaUnit.FootToTheFourth, "myQuantity", "Q");
+
+            // Act
+            // Assert
+            Assert.True(calcArea.Equals((object)calcArea));
+        }
+
+        [Fact]
+        public void EqualsNullObjectTest()
+        {
+            // Arrange
+            var calcArea = new CalcInertia(4.5, AreaMomentOfInertiaUnit.FootToTheFourth, "myQuantity", "Q");
+
+            // Act
+            // Assert
+            Assert.False(calcArea.Equals((object)null));
+        }
+
+        [Fact]
+        public void EqualsOtherObjectTest()
+        {
+            // Arrange
+            var calcArea1 = new CalcInertia(4.5, AreaMomentOfInertiaUnit.FootToTheFourth, "myQuantity", "Q");
+            var calcArea2 = new CalcInertia(4.5, AreaMomentOfInertiaUnit.FootToTheFourth, "myQuantity", "Q");
+
+            // Act
+            // Assert
+            Assert.True(calcArea1.Equals((object)calcArea2));
+        }
+
+        [Fact]
+        public void EqualsOtherTypeTest()
+        {
+            // Arrange
+            var calcArea = new CalcInertia(4.5, AreaMomentOfInertiaUnit.FootToTheFourth, "myQuantity", "Q");
+            var notCalcInertia = new CalcLength(4.5, LengthUnit.Foot, "length", "l");
+
+            // Act
+            // Assert
+            Assert.False(calcArea.Equals(notCalcInertia));
+        }
+
+        [Fact]
+        public void EqualsReferenceEqualsTest()
+        {
+            // Arrange
+            var calcArea = new CalcInertia(4.5, AreaMomentOfInertiaUnit.FootToTheFourth, "myQuantity", "Q");
+
+            // Act
+            // Assert
+            Assert.True(calcArea.Equals(calcArea));
+        }
+
+        [Fact]
+        public void EqualsNullTest()
+        {
+            // Arrange
+            var calcArea = new CalcInertia(4.5, AreaMomentOfInertiaUnit.FootToTheFourth, "myQuantity", "Q");
+
+            // Act
+            // Assert
+            Assert.False(calcArea.Equals(null));
+        }
+
+        [Fact]
+        public void EqualsOtherTest()
+        {
+            // Arrange
+            var calcArea1 = new CalcInertia(4.5, AreaMomentOfInertiaUnit.FootToTheFourth, "myQuantity", "Q");
+            var calcArea2 = new CalcInertia(4.5, AreaMomentOfInertiaUnit.FootToTheFourth, "myQuantity", "Q");
+
+            // Act
+            // Assert
+            Assert.True(calcArea1.Equals(calcArea2));
+        }
+
+        [Fact]
+        public void GetHashCodeTest()
+        {
+            // Arrange
+            var calcArea1 = new CalcInertia(4.5, AreaMomentOfInertiaUnit.FootToTheFourth, "myQuantity", "Q");
+            var calcArea2 = new CalcInertia(4.5, AreaMomentOfInertiaUnit.FootToTheFourth, "myQuantity", "Q");
+            var calcArea3 = new CalcInertia(4.5, AreaMomentOfInertiaUnit.FootToTheFourth, "MyQuantity", "Q");
+
+            // Act
+            bool firstEqualsSecond = calcArea1.GetHashCode() == calcArea2.GetHashCode();
+            bool firstEqualsThird = calcArea1.GetHashCode() == calcArea3.GetHashCode();
+
+            // Assert
+            Assert.True(firstEqualsSecond);
+            Assert.False(firstEqualsThird);
+        }
+
+        [Fact]
+        public void ValueAsStringTest()
+        {
+            // Arrange
+            var calcArea = new CalcInertia(4.5, AreaMomentOfInertiaUnit.FootToTheFourth, "myQuantity", "Q");
+
+            // Act
+            string value = calcArea.ValueAsString();
+
+            // Assert
+            Assert.Equal("4.5 ft⁴", value);
         }
     }
 }
