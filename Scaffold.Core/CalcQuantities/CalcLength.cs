d5bb20562aa17795f30735a9ab551c02eacb7087
kpne <kpn@kpne.dk>
2025-05-12 15:57:52 +0200

    Update CalcLength with Generic Math and tests

--- Scaffold.Core/CalcQuantities/CalcLength.cs
+++ Scaffold.Core/CalcQuantities/CalcLength.cs
@@ -1,9 +1,27 @@
-﻿using Scaffold.Core.Abstract;
+﻿#if NET7_0_OR_GREATER
+using System.Numerics;
+#endif
+using Scaffold.Core.Abstract;
 using Scaffold.Core.Static;
 
 namespace Scaffold.Core.CalcQuantities;
 
 public sealed class CalcLength : CalcQuantity<Length>
+#if NET7_0_OR_GREATER
+    , IParsable<CalcLength>
+    , IAdditionOperators<CalcLength, CalcLength, CalcLength>
+    , IAdditionOperators<CalcLength, double, CalcLength>
+    , IAdditiveIdentity<CalcLength, CalcLength>
+    , ISubtractionOperators<CalcLength, CalcLength, CalcLength>
+    , IMultiplyOperators<CalcLength, CalcLength, CalcArea>
+    , IMultiplyOperators<CalcLength, double, CalcLength>
+    , IMultiplyOperators<CalcLength, CalcArea, CalcVolume>
+    , IMultiplyOperators<CalcLength, CalcVolume, CalcInertia>
+    , IDivisionOperators<CalcLength, CalcLength, CalcStrain>
+    , IDivisionOperators<CalcLength, double, CalcLength>
+    , IUnaryNegationOperators<CalcLength, CalcLength>
+    , IComparisonOperators<CalcLength, CalcLength, bool>
+#endif
 {
     public CalcLength(Length length, string name, string symbol = "")
         : base(length, name, symbol) { }
@@ -11,12 +29,25 @@ public sealed class CalcLength : CalcQuantity<Length>
     public CalcLength(double value, LengthUnit unit, string name, string symbol)
         : base(new Length(value, unit), name, symbol) { }
 
-    public static CalcLength OneMeter => new CalcLength(Length.FromMeters(1), "Unit Length");
-
+    #region AdditionOperators
     public static CalcLength operator +(CalcLength x, CalcLength y)
     {
         (string name, string symbol, LengthUnit unit) = OperatorMetadataHelper<LengthUnit>(x, y, '+');
         return new CalcLength(new Length(x.Quantity.As(unit) + y.Quantity.As(unit), unit), name, symbol);
+    }
+
+    public static CalcLength operator +(CalcLength x, double y)
+    {
+        return new CalcLength(x.Value + y, (LengthUnit)x.Quantity.Unit, x.DisplayName, x.Symbol);
+    }
+
+    public static CalcLength operator +(double x, CalcLength y) => y + x;
+    #endregion
+
+    #region SubtractionOperators
+    public static CalcLength operator -(CalcLength x)
+    {
+        return new CalcLength(-(Length)x.Quantity, $"-{x.DisplayName}", x.Symbol);
     }
 
     public static CalcLength operator -(CalcLength x, CalcLength y)
@@ -25,12 +56,25 @@ public sealed class CalcLength : CalcQuantity<Length>
         return new CalcLength(new Length(x.Quantity.As(unit) - y.Quantity.As(unit), unit), name, symbol);
     }
 
+    public static CalcLength operator -(CalcLength x, double y)
+    {
+        return new CalcLength(x.Value - y, (LengthUnit)x.Quantity.Unit, x.DisplayName, x.Symbol);
+    }
+    #endregion
+
+    #region MultiplicationOperators
     public static CalcArea operator *(CalcLength x, CalcLength y)
     {
         (string name, string symbol, LengthUnit unit) = OperatorMetadataHelper<LengthUnit>(x, y, '·');
         return new CalcArea(new Area(x.Quantity.As(unit) * y.Quantity.As(unit),
             unit.GetEquivilantAreaUnit()), name, symbol);
     }
+    public static CalcLength operator *(CalcLength x, double y)
+    {
+        return new CalcLength(x.Value * y, (LengthUnit)x.Quantity.Unit, x.DisplayName, x.Symbol);
+    }
+
+    public static CalcLength operator *(double x, CalcLength y) => y * x;
 
     public static CalcVolume operator *(CalcLength x, CalcArea y)
     {
@@ -51,50 +95,80 @@ public sealed class CalcLength : CalcQuantity<Length>
             unit.GetEquivilantInertiaUnit()), name, "");
     }
 
+    public static CalcLength operator *(CalcStrain x, CalcLength y)
+    {
+        return new CalcLength(x.Quantity.As(StrainUnit.DecimalFraction) * (Length)y.Quantity, string.Empty, string.Empty);
+    }
+
+    public static CalcLength operator *(CalcLength x, CalcStrain y) => y * x;
+    #endregion
+
+    #region DivisionOperators
     public static CalcStrain operator /(CalcLength x, CalcLength y)
     {
         (string name, string _, LengthUnit unit) = OperatorMetadataHelper<LengthUnit>(x, y, '/');
         return new CalcStrain((Length)x.Quantity / (Length)y.Quantity, StrainUnit.DecimalFraction, name, string.Empty);
     }
 
-
-    public static CalcLength operator *(CalcStrain x, CalcLength y)
-    {
-        return new CalcLength(x.Quantity.As(StrainUnit.DecimalFraction) * (Length)y.Quantity, string.Empty, string.Empty);
-    }
-
-    public static CalcLength operator *(CalcLength x, CalcStrain y)
-    {
-        return y * x;
-    }
-
-    public static CalcLength operator +(CalcLength x, double y)
-    {
-        return new CalcLength(x.Value + y, (LengthUnit)x.Quantity.Unit, x.DisplayName, x.Symbol);
-    }
-
-    public static CalcLength operator +(double x, CalcLength y)
-    {
-        return y + x;
-    }
-
-    public static CalcLength operator -(CalcLength x, double y)
-    {
-        return new CalcLength(x.Value - y, (LengthUnit)x.Quantity.Unit, x.DisplayName, x.Symbol);
-    }
-
-    public static CalcLength operator *(CalcLength x, double y)
-    {
-        return new CalcLength(x.Value * y, (LengthUnit)x.Quantity.Unit, x.DisplayName, x.Symbol);
-    }
-
-    public static CalcLength operator *(double x, CalcLength y)
-    {
-        return y * x;
-    }
-
     public static CalcLength operator /(CalcLength x, double y)
     {
         return new CalcLength(x.Value / y, (LengthUnit)x.Quantity.Unit, x.DisplayName, x.Symbol);
     }
+    #endregion
+
+    #region PowerOperators
+    public static ICalcQuantity operator ^(CalcLength x, double y)
+    {
+        LengthUnit unit = (LengthUnit)x.Quantity.Unit;
+        string name;
+        switch (y)
+        {
+            case 2:
+                name = string.IsNullOrEmpty(x.DisplayName) ? string.Empty : $"{x.DisplayName}²";
+                return new CalcArea(Math.Pow(x.Value, y), unit.GetEquivilantAreaUnit(), name, "");
+
+            case 3:
+                name = string.IsNullOrEmpty(x.DisplayName) ? string.Empty : $"{x.DisplayName}³";
+                return new CalcVolume(Math.Pow(x.Value, y), unit.GetEquivilantVolumeUnit(), name, "");
+
+            case 4:
+                name = string.IsNullOrEmpty(x.DisplayName) ? string.Empty : $"{x.DisplayName}⁴";
+                return new CalcInertia(Math.Pow(x.Value, y), unit.GetEquivilantInertiaUnit(), name, "");
+        }
+
+        throw new MathException("CalcLength can only be raised by the power of 2, 3 or 4");
+    }
+    #endregion
+
+    #region ComparisonOperators
+    public static bool operator >(CalcLength left, CalcLength right) => GreaterThan(left, right);
+    public static bool operator >=(CalcLength left, CalcLength right) => GreaterOrEqualThan(left, right);
+    public static bool operator <(CalcLength left, CalcLength right) => LessThan(left, right);
+    public static bool operator <=(CalcLength left, CalcLength right) => LessOrEqualThan(left, right);
+    public static bool operator ==(CalcLength left, CalcLength right) => left.Equals(right);
+    public static bool operator !=(CalcLength left, CalcLength right) => !left.Equals(right);
+    #endregion
+
+    public static bool TryParse(string str, IFormatProvider provider, out CalcLength result)
+    {
+        if (Length.TryParse(str, provider, out Length quantity))
+        {
+            result = new CalcLength(quantity, string.Empty);
+            return true;
+        }
+
+        result = null;
+        return false;
+    }
+
+    public static CalcLength Parse(string str, IFormatProvider provider)
+    {
+        return new CalcLength(Length.Parse(str, provider), string.Empty);
+    }
+
+    internal static CalcLength OneMeter => new CalcLength(Length.FromMeters(1), "Unit Length");
+    public static CalcLength Zero => new CalcLength(Length.Zero, string.Empty);
+    public static CalcLength AdditiveIdentity => Zero;
+    public override bool Equals(object obj) => base.Equals(obj);
+    public override int GetHashCode() => base.GetHashCode();
 }
--- Scaffold.Tests/UnitTests/CalcQuantities/CalcLengthTests.cs
+++ Scaffold.Tests/UnitTests/CalcQuantities/CalcLengthTests.cs
@@ -1,4 +1,5 @@
 ﻿using Scaffold.Core.CalcQuantities;
+using Scaffold.Core.Exceptions;
 using UnitsNet;
 using UnitsNet.Units;
 
@@ -7,16 +8,40 @@ namespace Scaffold.Tests.UnitTests.CalcQuantities
     public class CalcLengthTests
     {
         [Fact]
-        public void ParseFromStringTest()
+        public void TryParseFromStringTest()
         {
             // Arrange
             var calcLength = new CalcLength(4.5, LengthUnit.Meter, "length", "L");
 
             // Act
             // Assert
-            Assert.True(calcLength.TryParse("5.5 cm"));
+            Assert.True(CalcLength.TryParse("5.5 cm", null, out calcLength));
             Assert.Equal(5.5, calcLength.Value);
             Assert.Equal("cm", calcLength.Unit);
+        }
+
+        [Fact]
+        public void ParseFromStringTest()
+        {
+            // Arrange
+            // Act
+            var calcLength = CalcLength.Parse("5.5 cm", null);
+
+            // Assert
+            Assert.Equal(5.5, calcLength.Value);
+            Assert.Equal("cm", calcLength.Unit);
+        }
+
+        [Fact]
+        public void TryParseFailureTest()
+        {
+            // Arrange
+            var calcQuantity = new CalcLength(4.5, LengthUnit.Meter, "length", "L");
+
+            // Act
+            // Assert
+            Assert.False(CalcLength.TryParse("two hundred horses", null, out calcQuantity));
+            Assert.Null(calcQuantity);
         }
 
         [Fact]
@@ -78,6 +103,22 @@ namespace Scaffold.Tests.UnitTests.CalcQuantities
             Assert.Equal("L", result.Symbol);
             Assert.Equal("cm", result.Unit);
             Assert.Equal("l1 - l2", result.DisplayName); // note: using Thin Space \u2009
+        }
+
+        [Fact]
+        public void UnaryNegationOperatorTest()
+        {
+            // Arrange
+            var calcLength = new CalcLength(4.5, LengthUnit.Centimeter, "a1", "A");
+
+            // Act
+            CalcLength result = -calcLength;
+
+            // Assert
+            Assert.Equal(-4.5, result.Value);
+            Assert.Equal("A", result.Symbol);
+            Assert.Equal("cm", result.Unit);
+            Assert.Equal("-a1", result.DisplayName);
         }
 
         [Fact]
@@ -214,6 +255,310 @@ namespace Scaffold.Tests.UnitTests.CalcQuantities
             Assert.Equal("cm", result.Unit);
             Assert.Equal("a1", result.DisplayName);
             Assert.Equal(4.5, calcLength.Value);
+        }
+
+        [Fact]
+        public void PowerOperator2Test()
+        {
+            // Arrange
+            var calcLength = new CalcLength(4.5, LengthUnit.Centimeter, "a1", "A");
+
+            // Act
+            CalcArea result = (CalcArea)(calcLength ^ 2);
+
+            // Assert
+            Assert.Equal(Math.Pow(4.5, 2), result.Value);
+            Assert.Equal("cm²", result.Unit);
+            Assert.Equal("a1²", result.DisplayName);
+            Assert.True(string.IsNullOrEmpty(result.Symbol));
+            Assert.Equal(4.5, calcLength.Value);
+        }
+
+        [Fact]
+        public void PowerOperator3Test()
+        {
+            // Arrange
+            var calcLength = new CalcLength(4.5, LengthUnit.Centimeter, "a1", "A");
+
+            // Act
+            CalcVolume result = (CalcVolume)(calcLength ^ 3);
+
+            // Assert
+            Assert.Equal(Math.Pow(4.5, 3), result.Value);
+            Assert.Equal("cm³", result.Unit);
+            Assert.Equal("a1³", result.DisplayName);
+            Assert.True(string.IsNullOrEmpty(result.Symbol));
+            Assert.Equal(4.5, calcLength.Value);
+        }
+
+        [Fact]
+        public void PowerOperator4Test()
+        {
+            // Arrange
+            var calcLength = new CalcLength(4.5, LengthUnit.Centimeter, "a1", "A");
+
+            // Act
+            CalcInertia result = (CalcInertia)(calcLength ^ 4);
+
+            // Assert
+            Assert.Equal(Math.Pow(4.5, 4), result.Value);
+            Assert.Equal("cm⁴", result.Unit);
+            Assert.Equal("a1⁴", result.DisplayName);
+            Assert.True(string.IsNullOrEmpty(result.Symbol));
+            Assert.Equal(4.5, calcLength.Value);
+        }
+
+        [Fact]
+        public void PowerOperatorDoubleExceptionTest()
+        {
+            // Arrange
+            var calcLength = new CalcLength(4.5, LengthUnit.Centimeter, "a1", "A");
+
+            // Act
+            // Assert
+            Assert.Throws<MathException>(() => calcLength ^ 0.4);
+        }
+
+        [Fact]
+        public void SumTest()
+        {
+            // Arrange
+            var calcLength1 = new CalcLength(1, LengthUnit.Centimeter, "a", "A");
+            var calcLength2 = new CalcLength(2, LengthUnit.Centimeter, "a", "A");
+            var calcLength3 = new CalcLength(3, LengthUnit.Centimeter, "a", "A");
+            var areas = new List<CalcLength>() { calcLength1, calcLength2, calcLength3 };
+
+            // Act
+            CalcLength sum = areas.Sum();
+
+            // Assert
+            Assert.Equal(6, sum.Value);
+            Assert.Equal("cm", sum.Unit);
+        }
+
+        [Fact]
+        public void AverageTest()
+        {
+            // Arrange
+            var calcLength1 = new CalcLength(1, LengthUnit.Centimeter, "a", "A");
+            var calcLength2 = new CalcLength(2, LengthUnit.Centimeter, "a", "A");
+            var calcLength3 = new CalcLength(3, LengthUnit.Centimeter, "a", "A");
+            var areas = new List<CalcLength>() { calcLength1, calcLength2, calcLength3 };
+
+            // Act
+            CalcLength sum = areas.Average();
+
+            // Assert
+            Assert.Equal(2, sum.Value);
+            Assert.Equal("cm", sum.Unit);
+        }
+
+        [Theory]
+        [InlineData(4.3, 4.3, true)]
+        [InlineData(4.31, 4.3, false)]
+        public void EqualOperatorTest(double val1, double val2, bool expected)
+        {
+            // Arrange
+            var calcLength1 = new CalcLength(val1, LengthUnit.Meter, "q2", "Q");
+            var calcLength2 = new CalcLength(val2 * 100, LengthUnit.Centimeter, "q2", "Q");
+
+            // Act
+            bool result = calcLength1 == calcLength2;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(4.3, 4.3, false)]
+        [InlineData(4.31, 4.3, true)]
+        public void NotEqualOperatorTest(double val1, double val2, bool expected)
+        {
+            // Arrange
+            var calcLength1 = new CalcLength(val1, LengthUnit.Meter, "q2", "Q");
+            var calcLength2 = new CalcLength(val2 * 100, LengthUnit.Centimeter, "q2", "Q");
+
+            // Act
+            bool result = calcLength1 != calcLength2;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(4.3, 4.3, false)]
+        [InlineData(4.31, 4.3, true)]
+        public void GreaterThanOperatorTest(double val1, double val2, bool expected)
+        {
+            // Arrange
+            var calcLength1 = new CalcLength(val1, LengthUnit.Meter, "q2", "Q");
+            var calcLength2 = new CalcLength(val2 * 100, LengthUnit.Centimeter, "q2", "Q");
+
+            // Act
+            bool result = calcLength1 > calcLength2;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(4.3, 4.3, false)]
+        [InlineData(4.3, 4.31, true)]
+        public void LessThanOperatorTest(double val1, double val2, bool expected)
+        {
+            // Arrange
+            var calcLength1 = new CalcLength(val1, LengthUnit.Meter, "q2", "Q");
+            var calcLength2 = new CalcLength(val2 * 100, LengthUnit.Centimeter, "q2", "Q");
+
+            // Act
+            bool result = calcLength1 < calcLength2;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(4.3, 4.3, true)]
+        [InlineData(4.31, 4.3, true)]
+        [InlineData(4.3, 4.31, false)]
+        public void GreaterOrEqualOperatorTest(double val1, double val2, bool expected)
+        {
+            // Arrange
+            var calcLength1 = new CalcLength(val1, LengthUnit.Meter, "q2", "Q");
+            var calcLength2 = new CalcLength(val2 * 100, LengthUnit.Centimeter, "q2", "Q");
+
+            // Act
+            bool result = calcLength1 >= calcLength2;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(4.3, 4.3, true)]
+        [InlineData(4.3, 4.31, true)]
+        [InlineData(4.31, 4.30, false)]
+        public void LessOrEqualOperatorTest(double val1, double val2, bool expected)
+        {
+            // Arrange
+            var calcLength1 = new CalcLength(val1, LengthUnit.Meter, "q2", "Q");
+            var calcLength2 = new CalcLength(val2 * 100, LengthUnit.Centimeter, "q2", "Q");
+
+            // Act
+            bool result = calcLength1 <= calcLength2;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void EqualsReferenceEqualsObjectTest()
+        {
+            // Arrange
+            var calcLength = new CalcLength(4.5, LengthUnit.Foot, "myQuantity", "Q");
+
+            // Act
+            // Assert
+            Assert.True(calcLength.Equals((object)calcLength));
+        }
+
+        [Fact]
+        public void EqualsNullObjectTest()
+        {
+            // Arrange
+            var calcLength = new CalcLength(4.5, LengthUnit.Foot, "myQuantity", "Q");
+
+            // Act
+            // Assert
+            Assert.False(calcLength.Equals((object)null));
+        }
+
+        [Fact]
+        public void EqualsOtherObjectTest()
+        {
+            // Arrange
+            var calcLength1 = new CalcLength(4.5, LengthUnit.Foot, "myQuantity", "Q");
+            var calcLength2 = new CalcLength(4.5, LengthUnit.Foot, "myQuantity", "Q");
+
+            // Act
+            // Assert
+            Assert.True(calcLength1.Equals((object)calcLength2));
+        }
+
+        [Fact]
+        public void EqualsOtherTypeTest()
+        {
+            // Arrange
+            var calcLength = new CalcLength(4.5, LengthUnit.Foot, "myQuantity", "Q");
+            var notCalcLength = new CalcArea(4.5, AreaUnit.SquareFoot, "length", "l");
+
+            // Act
+            // Assert
+            Assert.False(calcLength.Equals(notCalcLength));
+        }
+
+        [Fact]
+        public void EqualsReferenceEqualsTest()
+        {
+            // Arrange
+            var calcLength = new CalcLength(4.5, LengthUnit.Foot, "myQuantity", "Q");
+
+            // Act
+            // Assert
+            Assert.True(calcLength.Equals(calcLength));
+        }
+
+        [Fact]
+        public void EqualsNullTest()
+        {
+            // Arrange
+            var calcLength = new CalcLength(4.5, LengthUnit.Foot, "myQuantity", "Q");
+
+            // Act
+            // Assert
+            Assert.False(calcLength.Equals(null));
+        }
+
+        [Fact]
+        public void EqualsOtherTest()
+        {
+            // Arrange
+            var calcLength1 = new CalcLength(4.5, LengthUnit.Foot, "myQuantity", "Q");
+            var calcLength2 = new CalcLength(4.5, LengthUnit.Foot, "myQuantity", "Q");
+
+            // Act
+            // Assert
+            Assert.True(calcLength1.Equals(calcLength2));
+        }
+
+        [Fact]
+        public void GetHashCodeTest()
+        {
+            // Arrange
+            var calcLength1 = new CalcLength(4.5, LengthUnit.Foot, "myQuantity", "Q");
+            var calcLength2 = new CalcLength(4.5, LengthUnit.Foot, "myQuantity", "Q");
+            var calcLength3 = new CalcLength(4.5, LengthUnit.Foot, "MyQuantity", "Q");
+
+            // Act
+            bool firstEqualsSecond = calcLength1.GetHashCode() == calcLength2.GetHashCode();
+            bool firstEqualsThird = calcLength1.GetHashCode() == calcLength3.GetHashCode();
+
+            // Assert
+            Assert.True(firstEqualsSecond);
+            Assert.False(firstEqualsThird);
+        }
+
+        [Fact]
+        public void ValueAsStringTest()
+        {
+            // Arrange
+            var calcLength = new CalcLength(4.5, LengthUnit.Foot, "myQuantity", "Q");
+
+            // Act
+            string value = calcLength.ValueAsString();
+
+            // Assert
+            Assert.Equal("4.5 ft", value);
         }
     }
 }
