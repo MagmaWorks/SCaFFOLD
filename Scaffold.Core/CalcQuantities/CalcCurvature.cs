a19ab81f98d796bfa99d0aab442b03e8c1fee3be
kpne <kpn@kpne.dk>
2025-05-13 12:10:31 +0200

    CalcCurvature with tests

---
+++ Scaffold.Core/CalcQuantities/CalcCurvature.cs
@@ -0,0 +1,124 @@
+﻿#if NET7_0_OR_GREATER
+using System.Numerics;
+#endif
+using Scaffold.Core.Abstract;
+using Scaffold.Core.CalcValues;
+using Scaffold.Core.Static;
+
+namespace Scaffold.Core.CalcQuantities;
+
+public sealed class CalcCurvature : CalcQuantity<ReciprocalLength>
+#if NET7_0_OR_GREATER
+    , IParsable<CalcCurvature>
+    , IAdditionOperators<CalcCurvature, CalcCurvature, CalcCurvature>
+    , IAdditionOperators<CalcCurvature, double, CalcCurvature>
+    , IAdditiveIdentity<CalcCurvature, CalcCurvature>
+    , ISubtractionOperators<CalcCurvature, CalcCurvature, CalcCurvature>
+    , ISubtractionOperators<CalcCurvature, double, CalcCurvature>
+    , IMultiplyOperators<CalcCurvature, CalcLength, CalcDouble>
+    , IMultiplyOperators<CalcCurvature, double, CalcCurvature>
+    , IDivisionOperators<CalcCurvature, double, CalcCurvature>
+    , IDivisionOperators<CalcCurvature, CalcCurvature, CalcDouble>
+    , IUnaryNegationOperators<CalcCurvature, CalcCurvature>
+    , IComparisonOperators<CalcCurvature, CalcCurvature, bool>
+#endif
+{
+    public CalcCurvature(ReciprocalLength length, string name, string symbol = "")
+        : base(length, name, symbol) { }
+
+    public CalcCurvature(double value, ReciprocalLengthUnit unit, string name, string symbol)
+        : base(new ReciprocalLength(value, unit), name, symbol) { }
+
+    #region AdditionOperators
+    public static CalcCurvature operator +(CalcCurvature x, CalcCurvature y)
+    {
+        (string name, string symbol, ReciprocalLengthUnit unit) = OperatorMetadataHelper<ReciprocalLengthUnit>(x, y, '+');
+        return new CalcCurvature(new ReciprocalLength(x.Quantity.As(unit) + y.Quantity.As(unit), unit), name, symbol);
+    }
+
+    public static CalcCurvature operator +(CalcCurvature x, double y)
+    {
+        return new CalcCurvature(x.Value + y, (ReciprocalLengthUnit)x.Quantity.Unit, x.DisplayName, x.Symbol);
+    }
+
+    public static CalcCurvature operator +(double x, CalcCurvature y) => y + x;
+    #endregion
+
+    #region SubtractionOperators
+    public static CalcCurvature operator -(CalcCurvature x)
+    {
+        return new CalcCurvature(-(ReciprocalLength)x.Quantity, $"-{x.DisplayName}", x.Symbol);
+    }
+
+    public static CalcCurvature operator -(CalcCurvature x, CalcCurvature y)
+    {
+        (string name, string symbol, ReciprocalLengthUnit unit) = OperatorMetadataHelper<ReciprocalLengthUnit>(x, y, '-');
+        return new CalcCurvature(new ReciprocalLength(x.Quantity.As(unit) - y.Quantity.As(unit), unit), name, symbol);
+    }
+
+    public static CalcCurvature operator -(CalcCurvature x, double y)
+    {
+        return new CalcCurvature(x.Value - y, (ReciprocalLengthUnit)x.Quantity.Unit, x.DisplayName, x.Symbol);
+    }
+    #endregion
+
+    #region MultiplicationOperators
+    public static CalcDouble operator *(CalcCurvature x, CalcLength y)
+    {
+        string name = string.IsNullOrEmpty(x.DisplayName) || string.IsNullOrEmpty(y.DisplayName)
+            ? string.Empty : $"{x.DisplayName}\u2009·\u2009{y.DisplayName}";
+        LengthUnit unit = ((ReciprocalLengthUnit)x.Quantity.Unit).GetEquivilantLengthUnit();
+        return new CalcDouble(x.Value * y.Quantity.As(unit), name, string.Empty, "-");
+    }
+    public static CalcCurvature operator *(CalcCurvature x, double y)
+    {
+        return new CalcCurvature(x.Value * y, (ReciprocalLengthUnit)x.Quantity.Unit, x.DisplayName, x.Symbol);
+    }
+
+    public static CalcCurvature operator *(double x, CalcCurvature y) => y * x;
+    #endregion
+
+    #region DivisionOperators
+    public static CalcDouble operator /(CalcCurvature x, CalcCurvature y)
+    {
+        (string name, string _, ReciprocalLengthUnit _) = OperatorMetadataHelper<ReciprocalLengthUnit>(x, y, '/');
+        return new CalcDouble((ReciprocalLength)x.Quantity / (ReciprocalLength)y.Quantity, name, string.Empty, "-");
+    }
+
+    public static CalcCurvature operator /(CalcCurvature x, double y)
+    {
+        return new CalcCurvature(x.Value / y, (ReciprocalLengthUnit)x.Quantity.Unit, x.DisplayName, x.Symbol);
+    }
+    #endregion
+
+    #region ComparisonOperators
+    public static bool operator >(CalcCurvature left, CalcCurvature right) => GreaterThan(left, right);
+    public static bool operator >=(CalcCurvature left, CalcCurvature right) => GreaterOrEqualThan(left, right);
+    public static bool operator <(CalcCurvature left, CalcCurvature right) => LessThan(left, right);
+    public static bool operator <=(CalcCurvature left, CalcCurvature right) => LessOrEqualThan(left, right);
+    public static bool operator ==(CalcCurvature left, CalcCurvature right) => left.Equals(right);
+    public static bool operator !=(CalcCurvature left, CalcCurvature right) => !left.Equals(right);
+    #endregion
+
+    public static bool TryParse(string str, IFormatProvider provider, out CalcCurvature result)
+    {
+        if (ReciprocalLength.TryParse(str, provider, out ReciprocalLength quantity))
+        {
+            result = new CalcCurvature(quantity, string.Empty);
+            return true;
+        }
+
+        result = null;
+        return false;
+    }
+
+    public static CalcCurvature Parse(string str, IFormatProvider provider)
+    {
+        return new CalcCurvature(ReciprocalLength.Parse(str, provider), string.Empty);
+    }
+
+    public static CalcCurvature Zero => new CalcCurvature(ReciprocalLength.Zero, string.Empty);
+    public static CalcCurvature AdditiveIdentity => Zero;
+    public override bool Equals(object obj) => base.Equals(obj);
+    public override int GetHashCode() => base.GetHashCode();
+}
---
+++ Scaffold.Tests/UnitTests/CalcQuantities/CalcCurvatureTests.cs
@@ -0,0 +1,469 @@
+﻿using Scaffold.Core.CalcQuantities;
+using Scaffold.Core.CalcValues;
+using Scaffold.Core.Exceptions;
+using UnitsNet;
+using UnitsNet.Units;
+
+namespace Scaffold.Tests.UnitTests.CalcQuantities
+{
+    public class CalcCurvatureTests
+    {
+        [Fact]
+        public void TryParseFromStringTest()
+        {
+            // Arrange
+            var calcLength = new CalcCurvature(4.5, ReciprocalLengthUnit.InverseMeter, "length", "L");
+
+            // Act
+            // Assert
+            Assert.True(CalcCurvature.TryParse("5 cm⁻¹", null, out calcLength));
+            Assert.Equal(5, calcLength.Value);
+            Assert.Equal("cm⁻¹", calcLength.Unit);
+        }
+
+        [Fact]
+        public void ParseFromStringTest()
+        {
+            // Arrange
+            // Act
+            var calcLength = CalcCurvature.Parse("5.5 cm⁻¹", null);
+
+            // Assert
+            Assert.Equal(5.5, calcLength.Value);
+            Assert.Equal("cm⁻¹", calcLength.Unit);
+        }
+
+        [Fact]
+        public void TryParseFailureTest()
+        {
+            // Arrange
+            var calcQuantity = new CalcCurvature(4.5, ReciprocalLengthUnit.InverseMeter, "length", "L");
+
+            // Act
+            // Assert
+            Assert.False(CalcCurvature.TryParse("two hundred horses", null, out calcQuantity));
+            Assert.Null(calcQuantity);
+        }
+
+        [Fact]
+        public void ImplicitOperatorDoubleTest()
+        {
+            // Arrange
+            var calcLength = new CalcCurvature(4.5, ReciprocalLengthUnit.InverseMeter, "length", "L");
+
+            // Act
+            double value = calcLength;
+
+            // Assert
+            Assert.Equal(4.5, value);
+        }
+
+        [Fact]
+        public void ImplicitOperatorQuantityTest()
+        {
+            // Arrange
+            var calcLength = new CalcCurvature(4.5, ReciprocalLengthUnit.InverseMeter, "length", "L");
+
+            // Act
+            ReciprocalLength value = calcLength;
+
+            // Assert
+            Assert.Equal(4.5, value.InverseMeters);
+            Assert.Equal(ReciprocalLengthUnit.InverseMeter, value.Unit);
+        }
+
+        [Fact]
+        public void AdditionOperatorTest()
+        {
+            // Arrange
+            var calcLength1 = new CalcCurvature(4.5, ReciprocalLengthUnit.InverseCentimeter, "l1", "L");
+            var calcLength2 = new CalcCurvature(550, ReciprocalLengthUnit.InverseMeter, "l2", "L");
+
+            // Act
+            CalcCurvature result = calcLength1 + calcLength2;
+
+            // Assert
+            Assert.Equal(4.5 + 5.5, result.Value);
+            Assert.Equal("L", result.Symbol);
+            Assert.Equal("cm⁻¹", result.Unit);
+            Assert.Equal("l1 + l2", result.DisplayName); // note: using Thin Space \u2009
+        }
+
+        [Fact]
+        public void SubtractionOperatorTest()
+        {
+            // Arrange
+            var calcLength1 = new CalcCurvature(4.5, ReciprocalLengthUnit.InverseCentimeter, "l1", "L");
+            var calcLength2 = new CalcCurvature(550, ReciprocalLengthUnit.InverseMeter, "l2", "L");
+
+            // Act
+            CalcCurvature result = calcLength1 - calcLength2;
+
+            // Assert
+            Assert.Equal(-1, result.Value);
+            Assert.Equal("L", result.Symbol);
+            Assert.Equal("cm⁻¹", result.Unit);
+            Assert.Equal("l1 - l2", result.DisplayName); // note: using Thin Space \u2009
+        }
+
+        [Fact]
+        public void UnaryNegationOperatorTest()
+        {
+            // Arrange
+            var calcLength = new CalcCurvature(4.5, ReciprocalLengthUnit.InverseCentimeter, "a1", "A");
+
+            // Act
+            CalcCurvature result = -calcLength;
+
+            // Assert
+            Assert.Equal(-4.5, result.Value);
+            Assert.Equal("A", result.Symbol);
+            Assert.Equal("cm⁻¹", result.Unit);
+            Assert.Equal("-a1", result.DisplayName);
+        }
+
+        [Fact]
+        public void MultiplicationOperatorTest()
+        {
+            // Arrange
+            var calcCurvature = new CalcCurvature(4.5, ReciprocalLengthUnit.InverseCentimeter, "1/l1", "1/L");
+            var calcLength = new CalcLength(0.055, LengthUnit.Meter, "l2", "L");
+
+            // Act
+            CalcDouble result = calcCurvature * calcLength;
+
+            // Assert
+            Assert.Equal(4.5 * 5.5, result.Value);
+            Assert.Equal("-", result.Unit);
+            Assert.Equal("1/l1 · l2", result.DisplayName); // note: using Thin Space \u2009
+            Assert.True(string.IsNullOrEmpty(result.Symbol));
+        }
+
+        [Fact]
+        public void DivisionOperatorTest()
+        {
+            // Arrange
+            var calcLength1 = new CalcCurvature(4.5, ReciprocalLengthUnit.InverseCentimeter, "l1", "L");
+            var calcLength2 = new CalcCurvature(5.5, ReciprocalLengthUnit.InverseCentimeter, "l2", "L");
+
+            // Act
+            CalcDouble result = calcLength1 / calcLength2;
+
+            // Assert
+            Assert.Equal(4.5 / 5.5, result.Value, 12);
+            Assert.Equal("l1 / l2", result.DisplayName); // note: using Thin Space \u2009
+            Assert.Equal("-", result.Unit);
+            Assert.True(string.IsNullOrEmpty(result.Symbol));
+        }
+
+        [Fact]
+        public void AdditionDoubleOperatorTest()
+        {
+            // Arrange
+            var calcLength = new CalcCurvature(4.5, ReciprocalLengthUnit.InverseCentimeter, "a1", "A");
+
+            // Act
+            CalcCurvature result = 2.0 + calcLength;
+
+            // Assert
+            Assert.Equal(4.5 + 2, result.Value);
+            Assert.Equal("A", result.Symbol);
+            Assert.Equal("cm⁻¹", result.Unit);
+            Assert.Equal("a1", result.DisplayName);
+            Assert.Equal(4.5, calcLength.Value);
+        }
+
+        [Fact]
+        public void SubtractionDoubleOperatorTest()
+        {
+            // Arrange
+            var calcLength = new CalcCurvature(4.5, ReciprocalLengthUnit.InverseCentimeter, "a1", "A");
+
+            // Act
+            CalcCurvature result = calcLength - 2;
+
+            // Assert
+            Assert.Equal(4.5 - 2, result.Value);
+            Assert.Equal("A", result.Symbol);
+            Assert.Equal("cm⁻¹", result.Unit);
+            Assert.Equal("a1", result.DisplayName);
+            Assert.Equal(4.5, calcLength.Value);
+        }
+
+        [Fact]
+        public void MultiplicationDoubleOperatorTest()
+        {
+            // Arrange
+            var calcLength = new CalcCurvature(4.5, ReciprocalLengthUnit.InverseCentimeter, "a1", "A");
+
+            // Act
+            CalcCurvature result = 2.0 * calcLength;
+
+            // Assert
+            Assert.Equal(4.5 * 2, result.Value);
+            Assert.Equal("A", result.Symbol);
+            Assert.Equal("cm⁻¹", result.Unit);
+            Assert.Equal("a1", result.DisplayName);
+            Assert.Equal(4.5, calcLength.Value);
+        }
+
+        [Fact]
+        public void DivisionDoubleOperatorTest()
+        {
+            // Arrange
+            var calcLength = new CalcCurvature(4.5, ReciprocalLengthUnit.InverseCentimeter, "a1", "A");
+
+            // Act
+            CalcCurvature result = calcLength / 2;
+
+            // Assert
+            Assert.Equal(4.5 / 2, result.Value);
+            Assert.Equal("A", result.Symbol);
+            Assert.Equal("cm⁻¹", result.Unit);
+            Assert.Equal("a1", result.DisplayName);
+            Assert.Equal(4.5, calcLength.Value);
+        }
+
+        [Fact]
+        public void SumTest()
+        {
+            // Arrange
+            var calcLength1 = new CalcCurvature(1, ReciprocalLengthUnit.InverseCentimeter, "a", "A");
+            var calcLength2 = new CalcCurvature(2, ReciprocalLengthUnit.InverseCentimeter, "a", "A");
+            var calcLength3 = new CalcCurvature(3, ReciprocalLengthUnit.InverseCentimeter, "a", "A");
+            var areas = new List<CalcCurvature>() { calcLength1, calcLength2, calcLength3 };
+
+            // Act
+            CalcCurvature sum = areas.Sum();
+
+            // Assert
+            Assert.Equal(6, sum.Value);
+            Assert.Equal("cm⁻¹", sum.Unit);
+        }
+
+        [Fact]
+        public void AverageTest()
+        {
+            // Arrange
+            var calcLength1 = new CalcCurvature(1, ReciprocalLengthUnit.InverseCentimeter, "a", "A");
+            var calcLength2 = new CalcCurvature(2, ReciprocalLengthUnit.InverseCentimeter, "a", "A");
+            var calcLength3 = new CalcCurvature(3, ReciprocalLengthUnit.InverseCentimeter, "a", "A");
+            var areas = new List<CalcCurvature>() { calcLength1, calcLength2, calcLength3 };
+
+            // Act
+            CalcCurvature sum = areas.Average();
+
+            // Assert
+            Assert.Equal(2, sum.Value);
+            Assert.Equal("cm⁻¹", sum.Unit);
+        }
+
+        [Theory]
+        [InlineData(4.3, 4.3, true)]
+        [InlineData(4.31, 4.3, false)]
+        public void EqualOperatorTest(double val1, double val2, bool expected)
+        {
+            // Arrange
+            var calcLength1 = new CalcCurvature(val1, ReciprocalLengthUnit.InverseMeter, "q2", "Q");
+            var calcLength2 = new CalcCurvature(val2 / 100, ReciprocalLengthUnit.InverseCentimeter, "q2", "Q");
+
+            // Act
+            bool result = calcLength1 == calcLength2;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(4.3, 4.3, false)]
+        [InlineData(4.31, 4.3, true)]
+        public void NotEqualOperatorTest(double val1, double val2, bool expected)
+        {
+            // Arrange
+            var calcLength1 = new CalcCurvature(val1, ReciprocalLengthUnit.InverseMeter, "q2", "Q");
+            var calcLength2 = new CalcCurvature(val2 / 100, ReciprocalLengthUnit.InverseCentimeter, "q2", "Q");
+
+            // Act
+            bool result = calcLength1 != calcLength2;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(4.3, 4.3, false)]
+        [InlineData(4.31, 4.3, true)]
+        public void GreaterThanOperatorTest(double val1, double val2, bool expected)
+        {
+            // Arrange
+            var calcLength1 = new CalcCurvature(val1, ReciprocalLengthUnit.InverseMeter, "q2", "Q");
+            var calcLength2 = new CalcCurvature(val2 / 100, ReciprocalLengthUnit.InverseCentimeter, "q2", "Q");
+
+            // Act
+            bool result = calcLength1 > calcLength2;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(4.3, 4.3, false)]
+        [InlineData(4.3, 4.31, true)]
+        public void LessThanOperatorTest(double val1, double val2, bool expected)
+        {
+            // Arrange
+            var calcLength1 = new CalcCurvature(val1, ReciprocalLengthUnit.InverseMeter, "q2", "Q");
+            var calcLength2 = new CalcCurvature(val2 / 100, ReciprocalLengthUnit.InverseCentimeter, "q2", "Q");
+
+            // Act
+            bool result = calcLength1 < calcLength2;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(4.3, 4.3, true)]
+        [InlineData(4.31, 4.3, true)]
+        [InlineData(4.3, 4.31, false)]
+        public void GreaterOrEqualOperatorTest(double val1, double val2, bool expected)
+        {
+            // Arrange
+            var calcLength1 = new CalcCurvature(val1, ReciprocalLengthUnit.InverseMeter, "q2", "Q");
+            var calcLength2 = new CalcCurvature(val2 / 100, ReciprocalLengthUnit.InverseCentimeter, "q2", "Q");
+
+            // Act
+            bool result = calcLength1 >= calcLength2;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(4.3, 4.3, true)]
+        [InlineData(4.3, 4.31, true)]
+        [InlineData(4.31, 4.30, false)]
+        public void LessOrEqualOperatorTest(double val1, double val2, bool expected)
+        {
+            // Arrange
+            var calcLength1 = new CalcCurvature(val1, ReciprocalLengthUnit.InverseMeter, "q2", "Q");
+            var calcLength2 = new CalcCurvature(val2 / 100, ReciprocalLengthUnit.InverseCentimeter, "q2", "Q");
+
+            // Act
+            bool result = calcLength1 <= calcLength2;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void EqualsReferenceEqualsObjectTest()
+        {
+            // Arrange
+            var calcLength = new CalcCurvature(4.5, ReciprocalLengthUnit.InverseFoot, "myQuantity", "Q");
+
+            // Act
+            // Assert
+            Assert.True(calcLength.Equals((object)calcLength));
+        }
+
+        [Fact]
+        public void EqualsNullObjectTest()
+        {
+            // Arrange
+            var calcLength = new CalcCurvature(4.5, ReciprocalLengthUnit.InverseFoot, "myQuantity", "Q");
+
+            // Act
+            // Assert
+            Assert.False(calcLength.Equals((object)null));
+        }
+
+        [Fact]
+        public void EqualsOtherObjectTest()
+        {
+            // Arrange
+            var calcLength1 = new CalcCurvature(4.5, ReciprocalLengthUnit.InverseFoot, "myQuantity", "Q");
+            var calcLength2 = new CalcCurvature(4.5, ReciprocalLengthUnit.InverseFoot, "myQuantity", "Q");
+
+            // Act
+            // Assert
+            Assert.True(calcLength1.Equals((object)calcLength2));
+        }
+
+        [Fact]
+        public void EqualsOtherTypeTest()
+        {
+            // Arrange
+            var calcLength = new CalcCurvature(4.5, ReciprocalLengthUnit.InverseFoot, "myQuantity", "Q");
+            var notCalcCurvature = new CalcArea(4.5, AreaUnit.SquareFoot, "length", "l");
+
+            // Act
+            // Assert
+            Assert.False(calcLength.Equals(notCalcCurvature));
+        }
+
+        [Fact]
+        public void EqualsReferenceEqualsTest()
+        {
+            // Arrange
+            var calcLength = new CalcCurvature(4.5, ReciprocalLengthUnit.InverseFoot, "myQuantity", "Q");
+
+            // Act
+            // Assert
+            Assert.True(calcLength.Equals(calcLength));
+        }
+
+        [Fact]
+        public void EqualsNullTest()
+        {
+            // Arrange
+            var calcLength = new CalcCurvature(4.5, ReciprocalLengthUnit.InverseFoot, "myQuantity", "Q");
+
+            // Act
+            // Assert
+            Assert.False(calcLength.Equals(null));
+        }
+
+        [Fact]
+        public void EqualsOtherTest()
+        {
+            // Arrange
+            var calcLength1 = new CalcCurvature(4.5, ReciprocalLengthUnit.InverseFoot, "myQuantity", "Q");
+            var calcLength2 = new CalcCurvature(4.5, ReciprocalLengthUnit.InverseFoot, "myQuantity", "Q");
+
+            // Act
+            // Assert
+            Assert.True(calcLength1.Equals(calcLength2));
+        }
+
+        [Fact]
+        public void GetHashCodeTest()
+        {
+            // Arrange
+            var calcLength1 = new CalcCurvature(4.5, ReciprocalLengthUnit.InverseFoot, "myQuantity", "Q");
+            var calcLength2 = new CalcCurvature(4.5, ReciprocalLengthUnit.InverseFoot, "myQuantity", "Q");
+            var calcLength3 = new CalcCurvature(4.5, ReciprocalLengthUnit.InverseFoot, "MyQuantity", "Q");
+
+            // Act
+            bool firstEqualsSecond = calcLength1.GetHashCode() == calcLength2.GetHashCode();
+            bool firstEqualsThird = calcLength1.GetHashCode() == calcLength3.GetHashCode();
+
+            // Assert
+            Assert.True(firstEqualsSecond);
+            Assert.False(firstEqualsThird);
+        }
+
+        [Fact]
+        public void ValueAsStringTest()
+        {
+            // Arrange
+            var calcLength = new CalcCurvature(4.5, ReciprocalLengthUnit.InverseFoot, "myQuantity", "Q");
+
+            // Act
+            string value = calcLength.ValueAsString();
+
+            // Assert
+            Assert.Equal("4.5 ft⁻¹", value);
+        }
+    }
+}
