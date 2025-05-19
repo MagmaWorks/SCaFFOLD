585c2cd18548399874fd999840f3c3331678d096
kpne <kpn@kpne.dk>
2025-05-19 17:04:48 +0200

    LaTex symbols

--- Scaffold.Calculations/Eurocode/Concrete/ConcretePropertiesCalculation.cs
+++ Scaffold.Calculations/Eurocode/Concrete/ConcretePropertiesCalculation.cs
@@ -21,49 +21,49 @@ namespace Scaffold.Calculations.Eurocode.Concrete
         public CalcSelectionList ConcreteGrade { get; set; }
             = new CalcSelectionList("Concrete Grade", "C30/37", EnumSelectionListParser.ConcreteGrades);
 
-        [OutputCalcValue(@"CP", "Concrete Properties")]
+        [OutputCalcValue("CP", "Concrete Properties")]
         public ConcreteProperties ConcreteProperties { get; private set; }
 
-        [OutputCalcValue(@"fck", "Characteristic cylinder strength")]
+        [OutputCalcValue("f_{ck}", "Characteristic cylinder strength")]
         public CalcStress fck => ConcreteProperties.fck;
 
-        [OutputCalcValue(@"fck,cube", "Characteristic cube strength")]
+        [OutputCalcValue("f_{ck,cube}", "Characteristic cube strength")]
         public CalcStress fckcube => ConcreteProperties.fckcube;
 
-        [OutputCalcValue(@"fcm", "Mean cylinder strength")]
+        [OutputCalcValue("f_{cm}", "Mean cylinder strength")]
         public CalcStress fcm => ConcreteProperties.fcm;
 
-        [OutputCalcValue(@"fctm", "Mean tensile strength")]
+        [OutputCalcValue("f_{ctm}", "Mean tensile strength")]
         public CalcStress fctm => ConcreteProperties.fctm;
 
-        [OutputCalcValue(@"fctk,0.05", "Tensile strength 5% fractile")]
+        [OutputCalcValue("f_{ctk;0.05}", "Tensile strength 5% fractile")]
         public CalcStress fctk005 => ConcreteProperties.fctk005;
 
-        [OutputCalcValue(@"fctk,0.95", "Tensile strength 95% fractile")]
+        [OutputCalcValue("f_{ctk;0.95}", "Tensile strength 95% fractile")]
         public CalcStress fctk095 => ConcreteProperties.fctk095;
 
-        [OutputCalcValue(@"Ecm", "Secant modulus of elasticity")]
+        [OutputCalcValue("E_{cm}", "Secant modulus of elasticity")]
         public CalcStress Ecm => ConcreteProperties.Ecm;
 
-        [OutputCalcValue(@"εc1", "Nominal peak strain")]
+        [OutputCalcValue("ε_{c1}", "Nominal peak strain")]
         public CalcStrain Epsilonc1 => ConcreteProperties.Epsilonc1;
 
-        [OutputCalcValue(@"εcu1", "Nominal ultimate strain")]
+        [OutputCalcValue("ε_{cu1}", "Nominal ultimate strain")]
         public CalcStrain Epsiloncu1 => ConcreteProperties.Epsiloncu1;
 
-        [OutputCalcValue(@"εc2", "Simplified parabola-rectangle peak strain")]
+        [OutputCalcValue("ε_{c2}", "Simplified parabola-rectangle peak strain")]
         public CalcStrain Epsilonc2 => ConcreteProperties.Epsilonc2;
 
-        [OutputCalcValue(@"εcu2", "Simplified ultimate strain")]
+        [OutputCalcValue("ε_{cu2}", "Simplified ultimate strain")]
         public CalcStrain Epsiloncu2 => ConcreteProperties.Epsiloncu2;
 
-        [OutputCalcValue(@"n", "Exponent")]
+        [OutputCalcValue(@"\textit{n}", "Exponent")]
         public CalcDouble n => ConcreteProperties.n;
 
-        [OutputCalcValue(@"εc3", "Simplified bi-linear peak strain")]
+        [OutputCalcValue("ε_{c3}", "Simplified bi-linear peak strain")]
         public CalcStrain Epsilonc3 => ConcreteProperties.Epsilonc3;
 
-        [OutputCalcValue(@"εcu2", "Simplified ultimate strain")]
+        [OutputCalcValue("ε_{cu3}", "Simplified ultimate strain")]
         public CalcStrain Epsiloncu3 => ConcreteProperties.Epsiloncu3;
 
         public ConcretePropertiesCalculation()
--- Scaffold.Calculations/Eurocode/Concrete/Utility/ConcreteProperties.cs
+++ Scaffold.Calculations/Eurocode/Concrete/Utility/ConcreteProperties.cs
@@ -26,20 +26,20 @@ namespace Scaffold.Calculations.Eurocode.Concrete.Utility
         /// Characteristic cylinder strength
         /// </summary>
         public CalcStress fck
-            => new CalcStress(GetStrength(), "Characteristic cylinder strength", "f_ck");
+            => new CalcStress(GetStrength(), "Characteristic cylinder strength", "f_{ck}");
 
         /// <summary>
         /// Characteristic cube strength
         /// </summary>
         public CalcStress fckcube => new CalcStress(
             new Pressure(double.Parse(Material.Grade.ToString().Split('_')[1]), PressureUnit.Megapascal),
-            "Characteristic cube strength", "f_ck,cube");
+            "Characteristic cube strength", "f_{ck,cube}");
 
         /// <summary>
         /// Mean cylinder strength
         /// </summary>
         public CalcStress fcm => new CalcStress(GetStrength() + new Pressure(8, PressureUnit.Megapascal),
-                "Mean cylinder strength", "f_cm");
+                "Mean cylinder strength", "f_{cm}");
 
         /// <summary>
         /// Mean tensile strength
@@ -47,61 +47,61 @@ namespace Scaffold.Calculations.Eurocode.Concrete.Utility
         public CalcStress fctm => new CalcStress(fck <= 50
                     ? 0.3 * Math.Pow(fck, 2d / 3d)
                     : 2.12 * Math.Log(1 + fcm / 10),
-            PressureUnit.Megapascal, "Mean tensile strength", "f_ctm");
+            PressureUnit.Megapascal, "Mean tensile strength", "f_{ctm}");
 
         /// <summary>
         /// Tensile strength 5% fractile
         /// </summary>
         public CalcStress fctk005 => new CalcStress(0.7 * fctm,
-            PressureUnit.Megapascal, "Tensile strength 5% fractile", "f_ctk;0.05");
+            PressureUnit.Megapascal, "Tensile strength 5% fractile", "f_{ctk;0.05}");
 
         /// <summary>
         /// Tensile strength 95% fractile
         /// </summary>
         public CalcStress fctk095 => new CalcStress(1.3 * fctm,
-            PressureUnit.Megapascal, "Tensile strength 95% fractile", "f_ctk;0.95");
+            PressureUnit.Megapascal, "Tensile strength 95% fractile", "f_{ctk;0.95}");
 
         /// <summary>
         /// Secant modulus of elasticity 
         /// </summary>
         public CalcStress Ecm => new CalcStress(22 * Math.Pow(fcm / 10, 0.3),
-            PressureUnit.Gigapascal, "Secant modulus of elasticity", "E_cm");
+            PressureUnit.Gigapascal, "Secant modulus of elasticity", "E_{cm}");
 
         /// <summary>
         /// Compressive strain in the concrete at the peak stress fc
         /// </summary>
         public CalcStrain Epsilonc1 => new CalcStrain(Math.Min(2.8, 0.7 * Math.Pow(fcm, 0.31)),
-            RatioUnit.PartPerThousand, "Nominal peak strain", "ε_1");
+            RatioUnit.PartPerThousand, "Nominal peak strain", "ε_{c1}");
 
         public CalcStrain Epsiloncu1 => new CalcStrain(fck >= 50
                     ? 2.8 + 27.0 * Math.Pow((98 - fcm) / 100, 4)
                     : 3.5,
-            RatioUnit.PartPerThousand, "Nominal ultimate strain", "ε_cu1");
+            RatioUnit.PartPerThousand, "Nominal ultimate strain", "ε_{cu1}");
 
         public CalcStrain Epsilonc2 => new CalcStrain(fck >= 50
                     ? 2.0 + 0.085 * Math.Pow(fck - 50, 0.53)
                     : 2.0,
-            RatioUnit.PartPerThousand, "Simplified parabola-rectangle peak strain", "ε_c2");
+            RatioUnit.PartPerThousand, "Simplified parabola-rectangle peak strain", "ε_{c2}");
 
         public CalcStrain Epsiloncu2 => new CalcStrain(fck >= 50
                     ? 2.6 + 35.0 * Math.Pow((90 - fck) / 100, 4)
                     : 3.5,
-            RatioUnit.PartPerThousand, "Simplified ultimate strain", "ε_cu2");
+            RatioUnit.PartPerThousand, "Simplified ultimate strain", "ε_{cu2}");
 
         public CalcDouble n => new CalcDouble(fck >= 50
                     ? 1.4 + 23.4 * Math.Pow((90 - fck) / 100, 4)
                     : 2.0,
-             "Exponent", "n");
+             "Exponent", @"\textit{n}");
 
         public CalcStrain Epsilonc3 => new CalcStrain(fck >= 50
                     ? 1.75 + 0.55 * ((fck - 50) / 40)
                     : 1.75,
-            RatioUnit.PartPerThousand, "Simplified bi-linear peak strain", "ε_c3");
+            RatioUnit.PartPerThousand, "Simplified bi-linear peak strain", "ε_{c3}");
 
         public CalcStrain Epsiloncu3 => new CalcStrain(fck >= 50
                     ? 2.6 + 35.0 * Math.Pow((90 - fck) / 100, 4)
                     : 3.5,
-            RatioUnit.PartPerThousand, "Simplified ultimate strain", "ε_cu3");
+            RatioUnit.PartPerThousand, "Simplified ultimate strain", "ε_{cu3}");
 
         public ConcreteProperties(EnConcreteGrade grade, NationalAnnex nationalAnnex = NationalAnnex.UnitedKingdom)
         {
