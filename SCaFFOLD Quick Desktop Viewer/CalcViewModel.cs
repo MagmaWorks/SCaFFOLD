edd7a285afaf201687a0c696868854e373415e6b
Alex Baalham <a.baalham@whitbywood.com>
2025-05-26 11:12:09 +0100

    1st commit

---
+++ SCaFFOLD Quick Desktop Viewer/CalcViewModel.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Scaffold.Core;
+
+using Scaffold.Core.Interfaces;
+
+namespace SCaFFOLD_Quick_Desktop_Viewer
+{
+    internal class CalcViewModel
+    {
+        private ICalculation calculation { get; set; }
+
+        CalcViewModel(ICalculation calculation)
+        {
+            this.calculation = calculation;
+        }
+
+    }
+}
---
+++ SCaFFOLD Quick Desktop Viewer/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SCaFFOLD_Quick_Desktop_Viewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+    }
+}
