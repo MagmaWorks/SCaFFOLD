f2c335fea2a3adea1365ec88381d8ea6c24e6548
kpne <kpn@kpne.dk>
2025-05-26 18:18:09 +0200

    update code generator

--- TaxonomyObjectCreator/Program.cs
+++ TaxonomyObjectCreator/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
 
@@ -8,16 +9,23 @@ namespace TaxonomyObjectCodeGenerator
     {
         private static readonly List<string> _assemblies = new() {
             "MagmaWorks.Taxonomy.Profiles",
-            "MagmaWorks.Taxonomy.Materials"
+            "MagmaWorks.Taxonomy.Materials",
+            "MagmaWorks.Taxonomy.Sections",
+            "MagmaWorks.Taxonomy.Loads",
+            "MagmaWorks.Taxonomy.Cases",
         };
 
         private static readonly List<string> _globalUsings = new() {
+            "System",
+            "System.Collections.Generic",
+            "System.Globalization",
+            "System.Linq",
             "UnitsNet",
             "UnitsNet.Units",
         };
 
         private static readonly Dictionary<string, string> _renamings = new() {
-            { "Angle", "MagmaWorks.Taxonomy.Profiles.Angle" }
+            { "Angle", "MagmaWorks.Taxonomy.Profiles.Angle" },
         };
 
         private const string _namespace = "Scaffold.Core.CalcObjects";
@@ -29,6 +37,11 @@ namespace TaxonomyObjectCodeGenerator
                 List<Type> classes = GetAllPublicClassesFrom(assemblyName);
                 foreach (Type type in classes)
                 {
+                    if (type.BaseType.Name == "Exception")
+                    {
+                        continue;
+                    }
+
                     WriteClassToFile(type, assemblyName);
                 }
             }
@@ -59,24 +72,23 @@ namespace TaxonomyObjectCodeGenerator
         private static string GetPath(string assemblyName, string assembly)
         {
             DirectoryInfo? directory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent;
-            return Path.Combine(directory.FullName, "Scaffold.Core", "CalcObjects", assembly);
+            var paths = new List<string>()
+            {
+                directory.FullName, "Scaffold.Core", "CalcObjects", "GeneratedCode"
+            };
+            paths.AddRange(assembly.Split('.'));
+            return Path.Combine(paths.ToArray());
         }
 
         private static void WriteClassToFile(Type type, string assemblyName)
         {
-            string assembly = assemblyName.Split('.').LastOrDefault();
+            string assemblyBase = assemblyName.Replace(assemblyName.Split('.').LastOrDefault(), string.Empty);
+            string assembly = type.Namespace.Replace(assemblyBase, string.Empty);
             string filePath = GetPath(assemblyName, assembly);
             string name = type.Name;
 
+            var usings = new HashSet<string>();
             var sb = new StringBuilder();
-            sb.AppendLine($@"using {type.Namespace};
-using MagmaWorks.Taxonomy.Serialization;
-using Scaffold.Core.Abstract;");
-            if (_renamings.ContainsKey(name))
-            {
-                sb.AppendLine($@"using {name} = {_renamings[name]};");
-            }
-
             sb.AppendLine($@"
 namespace {_namespace}.{assembly};
 public sealed class Calc{name} : CalcTaxonomyObject<{name}>
@@ -91,9 +103,9 @@ public sealed class Calc{name} : CalcTaxonomyObject<{name}>
             foreach (ConstructorInfo constructor in constructors)
             {
                 ParameterInfo[] parameters = constructor.GetParameters();
-                string inputs = Inputs(parameters, type.Namespace);
+                string inputs = Inputs(parameters, type.Namespace, ref usings);
                 sb.AppendLine($@"
-    public Calc{name}({inputs}, string name, string symbol = """")
+    public Calc{name}({inputs}string name, string symbol = """")
         : base(new {name}({string.Join(", ", parameters.Select(s => s.Name).ToList())}), name, symbol) {{ }}");
             }
             sb.Append($@"
@@ -117,28 +129,91 @@ public sealed class Calc{name} : CalcTaxonomyObject<{name}>
     }}
 }}
 ");
+            sb.Insert(0, Usings(type, usings));
             Directory.CreateDirectory(filePath);
             var file = new StreamWriter($"{filePath}/Calc{name}.cs");
             file.Write(sb);
             file.Close();
         }
 
-        private static string Inputs(ParameterInfo[] parameters, string assemblyName)
+        private static string Inputs(ParameterInfo[] parameters, string assemblyName, ref HashSet<string> usings)
         {
             var sb = new StringBuilder();
             foreach (ParameterInfo parameter in parameters)
             {
                 string type = parameter.ParameterType.FullName;
+                string typeNamespace = parameter.ParameterType.Namespace;
+                type = type.Replace("Int32", "int");
+                type = type.Replace("Double", "double");
+                type = type.Replace("String", "string");
+                type = type.Replace("Boolean", "bool");
+
+                if (!_globalUsings.Contains(typeNamespace))
+                {
+                    usings.Add(typeNamespace);
+                }
+
                 foreach (string @using in _globalUsings)
                 {
                     type = type.Replace(@using, string.Empty);
                 }
 
                 type = type.Replace(assemblyName, string.Empty);
-                sb.Append($"{type.Trim('.')} {parameter.Name}, ");
+                if (type.StartsWith(".Collections.Generic"))
+                {
+                    type = type.Replace(".Collections.Generic.", string.Empty);
+                    type = type.Replace("`1[[", string.Empty);
+                    string listType = type.Split("List")[0];
+                    type = type.Split("List")[1].Split(',')[0].TrimEnd(' ');
+                    type = type.Split('.').LastOrDefault();
+                    type = $"{listType}List<{type.Trim('.')}>";
+                } else
+                {
+                    type = type.Split('.').LastOrDefault();
+                }
+
+                sb.Append($"{type} {parameter.Name}, ");
             }
 
-            return sb.ToString().TrimEnd(' ').TrimEnd(',');
+            string par = sb.ToString().TrimEnd(' ').Trim(',').Trim(' ');
+            if (string.IsNullOrEmpty(par))
+            {
+                return string.Empty;
+            }
+
+            return $"{par}, ";
+        }
+
+        private static string Usings(Type type, HashSet<string> usings)
+        {
+            var s = new List<string>()
+            {
+                "using MagmaWorks.Taxonomy.Serialization;",
+                "using Scaffold.Core.Abstract;",
+                $"using {type.Namespace};",
+            };
+
+            foreach (string use in usings)
+            {
+                if (!s.Contains($@"using {use};"))
+                {
+                    s.Add($@"using {use};");
+                }
+            }
+            
+            s.Sort();
+            if (_renamings.ContainsKey(type.Name))
+            {
+                s.Add($@"using {type.Name} = {_renamings[type.Name]};");
+            }
+
+            var sb = new StringBuilder();
+            foreach (string str in s)
+            {
+                sb.AppendLine(str);
+            }
+
+            return sb.ToString();
         }
     }
 }
