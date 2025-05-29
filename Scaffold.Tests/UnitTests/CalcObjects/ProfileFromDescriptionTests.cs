a1977bffd0f816b80a207f9ab185552490321539
kpne <kpn@kpne.dk>
2025-05-29 18:33:01 +0200

    profile description WIP

---
+++ Scaffold.Tests/UnitTests/CalcObjects/ProfileFromDescriptionTests.cs
@@ -0,0 +1,24 @@
+﻿using Scaffold.Core.Extensions;
+using Scaffold.Core.CalcObjects.Profiles;
+
+namespace Scaffold.Tests.UnitTests;
+
+public class ProfileFromDescriptionTests
+{
+    [Theory]
+    [InlineData("400 500 50 mm")]
+    [InlineData("40 cm 500 50 mm")]
+    [InlineData("0.4m 0.5m 50 mm")]
+    [InlineData("0.4 500 mm 0.05 m")]
+    public void Test(string description)
+    {
+        // Assemble
+        CalcRectangularHollow profile = default;
+
+        // Act
+        profile = profile.ProfileFromDescription(description); 
+
+        // Assert
+        Assert.Equal(400, profile.Width.Millimeters);
+    }
+}
