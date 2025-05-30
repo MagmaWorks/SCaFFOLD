4f8cb26d7d6a761f58c5cc0e5da878ff1b72e35d
kpne <kpn@kpne.dk>
2025-05-30 16:06:49 +0200

    profile string tests

--- Scaffold.Tests/UnitTests/CalcObjects/ProfileFromDescriptionTests.cs
+++ Scaffold.Tests/UnitTests/CalcObjects/ProfileFromDescriptionTests.cs
@@ -1,24 +1,25 @@
-﻿using Scaffold.Core.Extensions;
-using Scaffold.Core.CalcObjects.Profiles;
+﻿using Scaffold.Core.CalcObjects.Profiles;
 
 namespace Scaffold.Tests.UnitTests;
 
 public class ProfileFromDescriptionTests
 {
     [Theory]
-    [InlineData("400 500 50 mm")]
-    [InlineData("40 cm 500 50 mm")]
-    [InlineData("0.4m 0.5m 50 mm")]
-    [InlineData("0.4 500 mm 0.05 m")]
+    [InlineData("400 500 60 mm")]
+    [InlineData("40 cm 500 60 mm")]
+    [InlineData("0.4m 0.5m 60 mm")]
+    [InlineData("0.4 500 mm 0.06 m")]
+    [InlineData("40 x 50 x 6cm")]
+    [InlineData("40x50x6.0 cm")]
     public void Test(string description)
     {
         // Assemble
-        CalcRectangularHollow profile = default;
-
         // Act
-        profile = profile.ProfileFromDescription(description); 
+        CalcRectangularHollow profile = CalcRectangularHollow.CreateFromDescription(description);
 
         // Assert
         Assert.Equal(400, profile.Width.Millimeters);
+        Assert.Equal(500, profile.Height.Millimeters);
+        Assert.Equal(60, profile.Thickness.Millimeters);
     }
 }
