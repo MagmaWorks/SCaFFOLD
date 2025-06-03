6dffbe89e154d7324cd598e77c58e553015ec3a0
kpne <kpn@kpne.dk>
2025-06-03 18:06:28 +0200

    SectionProperties calc sample with test

---
+++ Scaffold Calculations Tests/Sections/SectionPropertiesTests.cs
@@ -0,0 +1,45 @@
+using MagmaWorks.Taxonomy.Profiles;
+using Scaffold.Core.CalcObjects.Profiles;
+using Scaffold.Core.CalcQuantities;
+
+namespace Scaffold.Calculations.Sections;
+
+public class SectionPropertiesTests
+{
+    private static CalculationReader Reader { get; } = new();
+
+    [Fact]
+    public void CalculationBaseSetupTest()
+    {
+        // Assemble
+        var calc = new SectionPropertiesCalculation();
+
+        // Act
+        CalculationMetadata metadata = Reader.GetMetadata(calc);
+        IReadOnlyList<ICalcValue> inputs = Reader.GetInputs(calc);
+        IReadOnlyList<ICalcValue> outputs = Reader.GetOutputs(calc);
+
+        // Assert
+        Assert.Equal("Section Properties Calculation", calc.ReferenceName);
+        Assert.Equal("Section Properties", calc.CalculationName);
+        Assert.Equal(1, inputs.Count);
+        Assert.Equal(11, outputs.Count);
+    }
+
+    [Theory]
+    [InlineData("500 x 800 cm", typeof(CalcRectangleProfile), "40m²")]
+    public void AreaTests(string description, Type profileType, string expectedArea)
+    {
+        // Assemble
+        object[] parameters = new object[] { description };
+        IProfile profile = (IProfile)profileType.GetMethod("CreateFromDescription").Invoke(null, parameters);
+        var calc = new SectionPropertiesCalculation();
+
+        // Act
+        calc.Profile = profile;
+        IReadOnlyList<ICalcValue> outputs = Reader.GetOutputs(calc);
+
+        // Assert
+        Assert.Equal(expectedArea, outputs[4].ValueAsString());
+    }
+}
---
+++ Scaffold.Calculations/Sections/SectionPropertiesCalculation.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using MagmaWorks.Taxonomy.Profiles;
+using Scaffold.Core.Attributes;
+using Scaffold.Core.CalcObjects.Profiles;
+using Scaffold.Core.CalcObjects.Sections;
+using Scaffold.Core.CalcQuantities;
+using Scaffold.Core.Enums;
+using Scaffold.Core.Interfaces;
+using UnitsNet;
+using UnitsNet.Units;
+
+namespace Scaffold.Calculations;
+public class SectionPropertiesCalculation : ICalculation
+{
+    public string ReferenceName { get; set; }
+    public string CalculationName { get; set; } = "Section Properties";
+    public CalcStatus Status { get; set; } = CalcStatus.None;
+
+    [InputCalcValue]
+    public IProfile Profile { get; set; }
+        = new CalcRectangleProfile(new Length(400, LengthUnit.Millimeter), new Length(800, LengthUnit.Millimeter), "400 x 800mm");
+
+    [OutputCalcValue("Section Properties", "SP")]
+    public CalcSectionProperties Properties => new CalcSectionProperties(Profile, ReferenceName);
+
+    [OutputCalcValue]
+    public CalcLength CentroidY => Properties.Centroid.Y;
+
+    [OutputCalcValue]
+    public CalcLength CentroidZ => Properties.Centroid.Z;
+
+    [OutputCalcValue]
+    public CalcLength Perimeter => Properties.Perimeter;
+
+    [OutputCalcValue]
+    public CalcArea Area => Properties.Area;
+
+    [OutputCalcValue]
+    public CalcVolume ElasticSectionModulusYy => Properties.ElasticSectionModulusYy;
+
+    [OutputCalcValue]
+    public CalcVolume ElasticSectionModulusZz => Properties.ElasticSectionModulusZz;
+
+    [OutputCalcValue]
+    public CalcInertia MomentOfInertiaYy => Properties.MomentOfInertiaYy;
+
+    [OutputCalcValue]
+    public CalcInertia MomentOfInertiaZz => Properties.MomentOfInertiaZz;
+
+    [OutputCalcValue]
+    public CalcLength RadiusOfGyrationYy => Properties.RadiusOfGyrationYy;
+
+    [OutputCalcValue]
+    public CalcLength RadiusOfGyrationZz => Properties.RadiusOfGyrationZz;
+
+    public List<IFormula> Expressions = new List<IFormula>();
+    public IList<IFormula> GetFormulae() => Expressions;
+
+    public SectionPropertiesCalculation()
+    {
+        Calculate();
+    }
+
+    public void Calculate() { }
+}
