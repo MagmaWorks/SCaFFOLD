f52cd7e7b4c3b296c5fcbec43c1593d202a42d02
kpne <kpn@kpne.dk>
2025-06-07 10:01:53 +0200

    Steel Cat Profile WIP

--- Scaffold.Calculations/CalculationUtility/CatalogueProfileSelectionList.cs
+++ Scaffold.Calculations/CalculationUtility/CatalogueProfileSelectionList.cs
@@ -5,8 +5,59 @@ using MagmaWorks.Taxonomy.Profiles;
 
 namespace Scaffold.Calculations.CalculationUtility
 {
-    internal static class CatalogueProfileSelectionListParser
+    internal enum CatalogueType
     {
+        IPE,
+        HE,
+        HEA,
+        HEAA,
+        HEB,
+        HEM,
+        HL,
+        HD,
+        HP,
+        UBP,
+        UB,
+        UC,
+        IPN,
+        J,
+        UPE,
+        PFC,
+        UPN,
+        CH,
+    }
+
+    internal static class CatalogueProfileSelectionList
+    {
+        internal static List<string> GetCatalogueProfiles(CatalogueType type)
+        {
+            switch (type)
+            {
+                case CatalogueType.IPE: return IPEs;
+                case CatalogueType.HE: return HEs;
+                case CatalogueType.HEA: return HEAs;
+                case CatalogueType.HEAA: return HEAAs;
+                case CatalogueType.HEB: return HEBs;
+                case CatalogueType.HEM: return HEMs;
+                case CatalogueType.HL: return HLs;
+                case CatalogueType.HD: return HDs;
+                case CatalogueType.HP: return HPs;
+                case CatalogueType.UBP: return UBPs;
+                case CatalogueType.UB: return UBs;
+                case CatalogueType.UC: return UCs;
+                case CatalogueType.IPN: return IPNs;
+                case CatalogueType.J: return Js;
+                case CatalogueType.UPE: return UPEs;
+                case CatalogueType.PFC: return PFCs;
+                case CatalogueType.UPN: return UPNs;
+                case CatalogueType.CH: return CHs;
+                default: throw new NotImplementedException();
+            }
+        }
+
+        internal static readonly List<string> Catalogues = Enum.GetValues(typeof(CatalogueType)).Cast<CatalogueType>()
+        .Select(p => p.ToString()).ToList();
+
         internal static readonly List<string> IPEs = Enum.GetValues(typeof(European)).Cast<European>()
         .Where(p => p.ToString().StartsWith("IPE")).Select(p => p.ToString()).ToList();
 
---
+++ Scaffold.Calculations/Eurocode/Steel/SteelCatalogueProfile.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using MagmaWorks.Taxonomy.Materials;
+using MagmaWorks.Taxonomy.Materials.StandardMaterials.En;
+using MagmaWorks.Taxonomy.Standards.Eurocode;
+using Scaffold.Calculations.CalculationUtility;
+using Scaffold.Core.Attributes;
+using Scaffold.Core.CalcObjects.Materials.StandardMaterials.En;
+using Scaffold.Core.CalcQuantities;
+using Scaffold.Core.CalcValues;
+using Scaffold.Core.Enums;
+using Scaffold.Core.Interfaces;
+using UnitsNet;
+using UnitsNet.Units;
+
+namespace Scaffold.Calculations.Eurocode.Steel
+{
+    public class SteelCatalogueProfile : ICalculation
+    {
+        public string ReferenceName { get; set; }
+        public string CalculationName { get; set; } = "Steel Catalogue Profile";
+        public CalcStatus Status { get; set; } = CalcStatus.None;
+
+        [InputCalcValue("Cat", "Catalogue")]
+        public CalcSelectionList CatalogueType { get; set; }
+            = new CalcSelectionList("Catalogue", "HEB", CatalogueProfileSelectionList.Catalogues);
+
+        [InputCalcValue]
+        public CalcLength Thickness { get; set; }
+            = new CalcLength(40, LengthUnit.Millimeter, "Nominal thickness of the element", "t");
+
+        [OutputCalcValue]
+        public CalcEnSteelMaterial Material =>
+            new CalcEnSteelMaterial(SteelGrade.GetEnum<EnSteelGrade>(),
+                                    NationalAnnex.RecommendedValues, "Steel", "S");
+
+
+        public SteelMaterialProperties()
+        {
+            Calculate();
+        }
+
+        public SteelMaterialProperties(EnSteelGrade grade)
+        {
+            SteelGrade.Value = grade.ToString();
+            Calculate();
+        }
+
+        public IList<IFormula> GetFormulae()
+        {
+            return new List<IFormula>();
+        }
+
+        public void Calculate() { }
+    }
+}
