79f93fc1f80d9348d6cb9d87e81573d3d06f5f11
kpne <kpn@kpne.dk>
2025-06-30 15:26:55 +0200

    CreateFaceReinforcementLayerByCount tests

---
+++ Scaffold.Calculations.Tests/Sections/Concrete/Reinforcement/CreateFaceReinforcementLayerByCountTests.cs
@@ -0,0 +1,66 @@
+﻿using MagmaWorks.Taxonomy.Sections;
+using MagmaWorks.Taxonomy.Sections.Reinforcement;
+using Scaffold.Core.CalcObjects.Sections.Reinforcement;
+using Scaffold.Core.CalcQuantities;
+
+namespace Scaffold.Calculations.Sections.Concrete.Reinforcement;
+
+public class CreateFaceReinforcementLayerByCountTests
+{
+    private static CalculationReader Reader { get; } = new();
+
+    [Fact]
+    public void CalculationBaseSetupTest()
+    {
+        // Assemble
+        var calc = new CreateFaceReinforcementLayerByCount();
+
+        // Act
+        CalculationMetadata metadata = Reader.GetMetadata(calc);
+        IReadOnlyList<ICalcValue> inputs = Reader.GetInputs(calc);
+        IReadOnlyList<ICalcValue> outputs = Reader.GetOutputs(calc);
+
+        // Assert
+        Assert.Equal("Create Face Reinforcement Layer By Count", calc.ReferenceName);
+        Assert.Equal("Create Face Reinforcement Layer By Count", calc.CalculationName);
+        Assert.Single(outputs);
+        Assert.Equal("Bottom Rebar 4 No.", outputs.FirstOrDefault().DisplayName);
+    }
+
+    [Theory]
+    [InlineData(0, typeof(CalcSelectionList), "Face", "Section Face")]
+    [InlineData(1, typeof(CalcRebar), "Bar", "Rebar")]
+    [InlineData(2, typeof(CalcInt), "No.", "Number of Bars")]
+    public void CalculationInputTests(int id, Type expectedType, string expectedSymbol, string expectedDisplayName)
+    {
+        // Assemble
+        var calc = new CreateFaceReinforcementLayerByCount();
+
+        // Act
+        IReadOnlyList<ICalcValue> inputs = Reader.GetInputs(calc);
+
+        // Assert
+        Assert.Equal(expectedType, inputs[id].GetType());
+        Assert.Equal(expectedSymbol, inputs[id].Symbol);
+        Assert.Equal(expectedDisplayName, inputs[id].DisplayName);
+    }
+
+    [Fact]
+    public void OutputIsExpectedTypeTest()
+    {
+        // Assemble
+        var calc = new CreateFaceReinforcementLayerByCount();
+
+        // Act
+        calc.SectionFace.Value = "Top";
+        calc.Rebar = new CreateRebar(BarDiameter.D16);
+        calc.Count = 6;
+
+        // Assert
+        Assert.IsType<CalcFaceReinforcementLayer>(calc.Output);
+        Assert.IsType<ReinforcementLayoutByCount>(calc.Output.Layout);
+        Assert.Equal(SectionFace.Top, calc.Output.Face);
+        Assert.Equal((double)BarDiameter.D16, calc.Output.Layout.Rebar.Diameter.Millimeters);
+        Assert.Equal(6, ((ReinforcementLayoutByCount)calc.Output.Layout).NumberOfBars);
+    }
+}
--- Scaffold.Calculations/Sections/Concrete/Reinforcement/CreateFaceReinforcementLayerByCount.cs
+++ Scaffold.Calculations/Sections/Concrete/Reinforcement/CreateFaceReinforcementLayerByCount.cs
@@ -8,6 +8,8 @@ using Scaffold.Core.CalcValues;
 namespace Scaffold.Calculations.Sections.Concrete.Reinforcement;
 public class CreateFaceReinforcementLayerByCount : CalcObjectInput<CalcFaceReinforcementLayer>
 {
+    public override string CalculationName { get; set; } = "Create Face Reinforcement Layer By Count";
+
     [InputCalcValue("Face", "Section Face")]
     public CalcSelectionList SectionFace { get; set; }
             = new CalcSelectionList("Section Face", "Bottom", EnumSelectionListParser.SectionFaces);
@@ -15,14 +17,14 @@ public class CreateFaceReinforcementLayerByCount : CalcObjectInput<CalcFaceReinf
     [InputCalcValue("Bar", "Rebar")]
     public CalcRebar Rebar { get; set; } = new CreateRebar();
 
-    [InputCalcValue("Count", "Number of Bars")]
-    public int Count { get; set; } = 4;
+    [InputCalcValue("No.", "Number of Bars")]
+    public CalcInt Count { get; set; } = 4;
 
     public CreateFaceReinforcementLayerByCount() { }
 
     protected override CalcFaceReinforcementLayer InitialiseOutput()
     {
         SectionFace face = SectionFace.GetEnum<SectionFace>();
-        return new CalcFaceReinforcementLayer(face, Rebar, Count, ReferenceName ?? string.Empty);
+        return new CalcFaceReinforcementLayer(face, Rebar, Count, ReferenceName ?? $"{SectionFace.Value} {Rebar.DisplayName} {Count}No.");
     }
 }
